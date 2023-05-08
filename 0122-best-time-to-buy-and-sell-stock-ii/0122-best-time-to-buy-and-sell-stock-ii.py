86c388aff99eec411306412c2dcccc8cff7c771e
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-05-09 01:08:24 +0530

    Time: 49 ms (22.21%), Space: 14.4 MB (89.59%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,26 +1,30 @@
 class Solution(object):
-    def maxProfit(self, prices):
+    def maxProfit(self, p):
+        """
+        One-Liner
+        """
+        return sum(p[i] - p[i - 1] for i in range(1, len(p)) if p[i] > p[i - 1])
         """
         Greedy Approach
         """
         res = 0
-        for i in range(1, len(prices)):
-            if prices[i - 1] < prices[i]:
-                res += prices[i] - prices[i - 1]
+        for i in range(1, len(p)):
+            if p[i - 1] < p[i]:
+                res += p[i] - p[i - 1]
         return res
         """
         Recursive + DP + Memoization
         """
-        dp = [[-1, -1] for _ in prices]
+        dp = [[-1, -1] for _ in p]
         def dfs(i, buy):
-            if i == len(prices): return 0
+            if i == len(p): return 0
             if dp[i][buy] != -1: return dp[i][buy]
             if buy:
                 dontSell = dfs(i + 1, buy)
-                sell = prices[i] + dfs(i + 1, 0)
+                sell = p[i] + dfs(i + 1, 0)
             else:
                 dontSell = dfs(i + 1, buy)
-                sell = -prices[i] + dfs(i + 1, 1)
+                sell = -p[i] + dfs(i + 1, 1)
             dp[i][buy] = max(dontSell, sell)
             return dp[i][buy] 
         return dfs(0, 0)
