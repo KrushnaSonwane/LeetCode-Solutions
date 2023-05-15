f1ec22d957d99fb3944c666f218665fdea3b26ec
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-05-15 16:51:27 +0530

    Time: 36 ms (5.59%), Space: 14.3 MB (5.59%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -4,17 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        dp = {}
+        dp = [[-1 for _ in s] for _ in s]
         def dfs(i, rem):
-            if i == len(s):
-                return rem == 0
-            if (i, rem) in dp: return dp[(i, rem)]
+            if i == len(s): return rem == 0
+            if dp[i][rem] != -1: return dp[i][rem]
             if s[i] == '(':
                 ans = dfs(i + 1, rem + 1)
             elif s[i] == ')':
                 ans = rem != 0 and dfs(i + 1, rem - 1)
             else:
                 ans = (rem != 0 and dfs(i + 1, rem - 1)) or dfs(i + 1, rem + 1) or dfs(i + 1, rem)
-            dp[(i, rem)] = ans
+            dp[i][rem] = ans
             return ans
         return dfs(0, 0)
