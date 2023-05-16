b4a3f1afec3940697e1e4a762fe74f854e02f91c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-05-16 18:58:23 +0530

    Time: 152 ms (94.74%), Space: 16.1 MB (46.32%) - LeetHub

--- 2662-minimum-cost-of-a-path-with-special-roads/2662-minimum-cost-of-a-path-with-special-roads.py
+++ 2662-minimum-cost-of-a-path-with-special-roads/2662-minimum-cost-of-a-path-with-special-roads.py
@@ -1,19 +1,11 @@
 class Solution(object):
     def minimumCost(self, start, target, specialRoads):
-        """
-        :type start: List[int]
-        :type target: List[int]
-        :type specialRoads: List[List[int]]
-        :rtype: int
-        """
         useIt = []
         for x1, y1, x2, y2, c in specialRoads:
             currCost = abs(x1 - x2) + abs(y1 - y2)
             if currCost > c:
                 useIt.append([x1, y1, x2, y2, c])
-        dist = {}
-        x, y = start
-        queue = [[0, x, y]]
+        queue, dist = [[0, start[0], start[1]]], {}
         heapq.heapify(queue)
         while queue:
             cost, x1, y1 = heapq.heappop(queue)
