6b3fc99df6c977841eacb019904ed5aa37ade5ca
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-05-24 17:43:42 +0530

    Time: 631 ms (17.74%), Space: 28.3 MB (8.06%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def eventualSafeNodes(self, graph):
+        n = len(graph)
+        path, visit = [0] * n, [0] * n
+        adj = defaultdict(list)
+        for i in range(n):
+            for node in graph[i]:
+                adj[i].append(node)
+        
+        def cycle(node):
+            if visit[node] and path[node]: return True
+            if visit[node]: return False
+            visit[node] = 1
+            path[node] = 1
+            for child in adj[node]:
+                if cycle(child):
+                    return True
+            path[node] = 0
+            return False
+
+        for i in range(n):
+            if not visit[i]:
+                cycle(i)
+        return [i for i in range(n) if not path[i]]
