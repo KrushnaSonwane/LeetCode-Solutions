ed1411e9d4150772efc8c0805ebebc7e25d88b9e
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-05-25 19:18:54 +0530

    Time: 4353 ms (56.52%), Space: 13.4 MB (82.61%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -7,13 +7,8 @@ class Solution(object):
         m, n = len(grid), len(grid[0])
         def dfs(r, c):
             if r in [-1, m] or c in [-1, n] or not grid[r][c]: return 0
-            temp = grid[r][c]
-            grid[r][c] = 0
+            temp, grid[r][c] = grid[r][c], 0
             max_ = max(dfs(r-1,c), dfs(r+1,c), dfs(r,c-1), dfs(r,c+1)) + temp
             grid[r][c] = temp
             return max_
-        res = 0
-        for i in range(m):
-            for j in range(n):
-                res = max(res, dfs(i, j))
-        return res
+        return max(dfs(i, j) for i in range(m) for j in range(n))
