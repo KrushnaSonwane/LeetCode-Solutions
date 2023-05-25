d1191bba5cfd46b4a83dcb1c93bd9c6cac17d294
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-05-25 19:17:49 +0530

    Time: 4446 ms (47.83%), Space: 13.4 MB (82.61%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def getMaximumGold(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+        def dfs(r, c):
+            if r in [-1, m] or c in [-1, n] or not grid[r][c]: return 0
+            temp = grid[r][c]
+            grid[r][c] = 0
+            max_ = max(dfs(r-1,c), dfs(r+1,c), dfs(r,c-1), dfs(r,c+1)) + temp
+            grid[r][c] = temp
+            return max_
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                res = max(res, dfs(i, j))
+        return res
