372eeb7496628c5f828854f1b864692ed908179c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-03 13:54:17 +0530

    Time: 2600 ms (34.84%), Space: 28.9 MB (15.88%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -2,13 +2,11 @@ class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         m, n = len(box), len(box[0])
         for i in range(m):
-            ptr1, ptr2 = n - 1, n - 1
-            while ptr1 >= 0:
-                if box[i][ptr1] == '*':
-                    ptr2 = ptr1 - 1
-                if box[i][ptr1] == '#':
-                    box[i][ptr1] = '.'
-                    box[i][ptr2] = '#'
-                    ptr2 -= 1
-                ptr1 -= 1
+            ptr = n - 1
+            for j in range(n - 1, -1, -1):
+                if box[i][j] == '*': ptr = j - 1
+                elif box[i][j] == '#':
+                    box[i][j] = '.'
+                    box[i][ptr] = '#'
+                    ptr -= 1
         return [[box[j][i] for j in range(m)][::-1] for i in range(n)]
