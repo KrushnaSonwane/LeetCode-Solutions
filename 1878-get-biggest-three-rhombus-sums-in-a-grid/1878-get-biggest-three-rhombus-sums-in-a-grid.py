998e6708e7ad9b8f85ecfbac6736e152b48255eb
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-04 17:17:42 +0530

    Time: 1589 ms (39.78%), Space: 17.6 MB (64.52%) - LeetHub

--- 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.py
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.py
@@ -6,26 +6,24 @@ class Solution:
             for j in range(n):
                 size = 0
                 while i - size >= 0 and i + size < m and j - size >= 0 and j + size < n:
-                    if not size: res.append(grid[i][j])
-                    else:
-                        temp = j
-                        sum_ = 0
-                        for k in range(i - size, i):
-                            sum_ += grid[k][temp]
-                            temp -= 1
-                        temp = i
-                        for k in range(j - size, j):
-                            sum_ += grid[temp][k]
-                            temp += 1
-                        temp = j
-                        for k in range(i + size, i, -1):
-                            sum_ += grid[k][temp]
-                            temp += 1
-                        temp = i
-                        for k in range(j + size, j, -1):
-                            sum_ += grid[temp][k]
-                            temp -= 1
-                        res.append(sum_)
+                    temp = j
+                    sum_ = 0
+                    for k in range(i - size, i):
+                        sum_ += grid[k][temp]
+                        temp -= 1
+                    temp = i
+                    for k in range(j - size, j):
+                        sum_ += grid[temp][k]
+                        temp += 1
+                    temp = j
+                    for k in range(i + size, i, -1):
+                        sum_ += grid[k][temp]
+                        temp += 1
+                    temp = i
+                    for k in range(j + size, j, -1):
+                        sum_ += grid[temp][k]
+                        temp -= 1
+                    res.append(max(sum_, grid[i][j]))
                     size += 1
         res.sort()
         arr = set()
