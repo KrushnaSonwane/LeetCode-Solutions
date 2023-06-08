327b64bfdbc04dcee5e764a0d842f27e2996ad5d
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-08 22:54:34 +0530

    Time: 47 ms (53.75%), Space: 16.2 MB (70.90%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        hashT = defaultdict(int)
-        for ch in text: hashT[ch] += 1
+        hashT = Counter(text)
         hashT['l'] //= 2; hashT['o'] //= 2
         return min(hashT[ch] for ch in 'balon')
