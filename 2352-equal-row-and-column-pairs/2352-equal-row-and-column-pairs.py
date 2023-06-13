6955e86cf997238b5f1a5aad535cce3e868da5e0
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-13 22:34:00 +0530

    Time: 619 ms (38.82%), Space: 22.2 MB (11.23%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,9 +1,6 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        res, n = 0, len(grid)
-        hashT = defaultdict(int)
+        n, hashT = len(grid), defaultdict(int)
         for i in range(n):
             hashT['.'.join(str(grid[i][j]) for j in range(n))] += 1
-        for i in range(n):
-            res += hashT['.'.join(str(grid[j][i]) for j in range(n))]
-        return res
+        return sum(hashT['.'.join(str(grid[j][i]) for j in range(n))] for i in range(n))
