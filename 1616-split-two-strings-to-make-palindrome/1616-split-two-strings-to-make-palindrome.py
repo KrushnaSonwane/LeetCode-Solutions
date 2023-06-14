7499f87fd776dde2e1e9807c63c1cbbbed9ea7bc
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-15 01:02:53 +0530

    Time: 148 ms (18.28%), Space: 17.9 MB (32.26%) - LeetHub

---
+++ 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkPalindromeFormation(self, a: str, b: str) -> bool:
+        def solve(a, b):
+            l, r = 0, len(a) - 1
+            while r > l:
+                if a[l] != b[r]:
+                    t1, t2 = l, r
+                    while r > l:
+                        if a[l] != a[r]: break
+                        l += 1
+                        r -= 1
+                    else:
+                        return True
+                    l, r = t1, t2
+                    while r > l:
+                        if b[l] != b[r]: break
+                        l += 1
+                        r -= 1
+                    else: return True
+                    break
+                l += 1
+                r -= 1
+            else:
+                return 1
+            return 0
+        return solve(a, b) or solve(b, a)
