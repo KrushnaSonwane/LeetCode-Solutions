1616bf9d9080ece34782f52a66889e8a94fb3547
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-15 19:00:15 +0530

    Time: 7283 ms (13.49%), Space: 16.3 MB (81.54%) - LeetHub

---
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProduct(self, s: str) -> int:
+        def dfs(i, a, b):
+            if i == len(s):
+                if a == a[::-1] and b == b[::-1]:
+                    self.res = max(self.res, len(b) * len(a))
+                return
+            dfs(i + 1, a, b)
+            dfs(i + 1, a + s[i], b)
+            dfs(i + 1, a, b + s[i])
+        self.res = 0
+        dfs(0, '', '')
+        return self.res
