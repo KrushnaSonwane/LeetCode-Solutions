0c0909f24afa4a568929e75015de140feac28ea8
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-16 23:33:05 +0530

    Time: 658 ms (56.02%), Space: 21 MB (58.43%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -1,30 +1,11 @@
 class Solution:
     def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
         res = n * 2
-        hashT = defaultdict(list)
-        for row, seat in reservedSeats: hashT[row].append(seat)
+        hashT = defaultdict(set)
+        for row, seat in reservedSeats:
+            if seat in [2, 3, 4, 5]: hashT[row].add(1)
+            if seat in [4, 5, 6, 7]: hashT[row].add(2)
+            if seat in [6, 7, 8, 9]: hashT[row].add(3)
         for row in hashT:
-            if 2 in hashT[row] or 3 in hashT[row]:
-                res -= 1
-                if 8 in hashT[row] or 9 in hashT[row]:
-                    for i in [4, 5, 6, 7]:
-                        if i in hashT[row]:
-                            res -= 1; break
-                else:
-                    if 6 in hashT[row] or 7 in hashT[row]:
-                        res -= 1
-            elif 8 in hashT[row] or 9 in hashT[row]:
-                res -= 1
-                if 2 in hashT[row] or 3 in hashT[row]:
-                    for i in [4, 5, 6, 7]:
-                        if i in hashT[row]:
-                            res -= 1; break
-                else:
-                    if 4 in hashT[row] or 5 in hashT[row]:
-                        res -= 1
-            else:
-                if 4 in hashT[row] or 5 in hashT[row]:
-                    res -= 1
-                if 6 in hashT[row] or 7 in hashT[row]:
-                    res -= 1
+            res -= 2 if len(hashT[row]) == 3 else 1
         return res
