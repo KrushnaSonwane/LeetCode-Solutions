cf5e21cb012319eb09e9f455e15bd5623366dcb1
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-17 18:07:10 +0530

    Time: 1264 ms (99.13%), Space: 55.2 MB (66.96%) - LeetHub

--- 2080-range-frequency-queries/2080-range-frequency-queries.py
+++ 2080-range-frequency-queries/2080-range-frequency-queries.py
@@ -1,15 +1,11 @@
+import bisect
 class RangeFreqQuery:
-
     def __init__(self, arr: List[int]):
         self.hashT = defaultdict(list)
-        for i, num in enumerate(arr):
-            self.hashT[num].append(i)
+        for i, num in enumerate(arr): self.hashT[num].append(i)
 
-    def query(self, left: int, right: int, value: int) -> int:
-        if not self.hashT[value]: return 0
-        l = bisect.bisect_left(self.hashT[value], left)
-        r = bisect.bisect_right(self.hashT[value], right)
-        return r - l
+    def query(self, l: int, r: int, val: int) -> int:
+        return bisect_right(self.hashT[val], r) - bisect_left(self.hashT[val], l)
 
 # Your RangeFreqQuery object will be instantiated and called as such:
 # obj = RangeFreqQuery(arr)
