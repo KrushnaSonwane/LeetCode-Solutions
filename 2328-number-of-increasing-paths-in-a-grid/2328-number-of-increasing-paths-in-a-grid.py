c9116c5faa28795f2b699269fc3dd98ed68cd8c6
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-18 13:07:02 +0530

    Time: 2430 ms (51.46%), Space: 110.4 MB (37.87%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -1,16 +1,14 @@
 class Solution:
     def countPaths(self, grid: List[List[int]]) -> int:
-        dp = {}
         m, n = len(grid), len(grid[0])
+        @cache
         def dfs(i, j):
             if i in [-1, m] or j in [-1, n]: return 0
-            if (i, j) not in dp:
-                res = 1
-                for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-                    if x in [-1, m] or y in [-1, n] or grid[x][y] <= grid[i][j]: continue
-                    res += dfs(x, y)
-                dp[(i, j)] = res
-            return dp[(i, j)]
+            res = 1
+            for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                if x in [-1, m] or y in [-1, n] or grid[x][y] <= grid[i][j]: continue
+                res += dfs(x, y)
+            return res
         ans = 0
         for r in range(m):
             for c in range(n):
