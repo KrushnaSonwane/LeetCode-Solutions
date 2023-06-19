9ab15b1ebcad89a4fc7edfdddad665b9189ed6eb
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-19 08:14:09 +0530

    Time: 43 ms (87.51%), Space: 16.3 MB (66.27%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,10 +1,6 @@
-class Solution(object):
-    def largestAltitude(self, gain):
-        """
-        :type gain: List[int]
-        :rtype: int
-        """
-        res, sum_ = 0, 0
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        res = sum_ = 0
         for num in gain:
             sum_ += num
             res = max(res, sum_)
