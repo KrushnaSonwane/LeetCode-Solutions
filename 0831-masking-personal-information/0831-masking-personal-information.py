bb1ce3ef494f569ded8d79d2fa2594284f89e02b
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-19 17:15:57 +0530

    Time: 47 ms (48.53%), Space: 16.3 MB (20.59%) - LeetHub

--- 0831-masking-personal-information/0831-masking-personal-information.py
+++ 0831-masking-personal-information/0831-masking-personal-information.py
@@ -2,26 +2,18 @@ class Solution:
     def maskPII(self, s: str) -> str:
         res = ''
         if "@" in s:
-            res += s[0].lower() + '*****'
-            ptr1 = 1
-            while s[ptr1] != '@':
-                ptr1 += 1
-            res += s[ptr1 - 1].lower() + '@'
-            ptr1 += 1
-            while len(s) > ptr1: 
-                res += s[ptr1].lower()
-                ptr1 += 1
+            res += s[0].lower()
+            i = s.index("@")
+            res += '*****' + s[i - 1].lower() + s[i]
+            res += ''.join(ch.lower() for ch in s[i + 1: ])
         else:
             number = []
             for ch in s:
                 if ch.isdigit(): number.append(ch)
             n = len(number)
-            if n == 10:
-                res += "***-***-"
-            elif n == 11:
-                res += "+*-***-***-"
-            elif n == 12:
-                res += "+**-***-***-"
+            if n == 10: res += "***-***-"
+            elif n == 11: res += "+*-***-***-"
+            elif n == 12: res += "+**-***-***-"
             else: res += "+***-***-***-"
             res += ''.join(number[-4:])
         return res
