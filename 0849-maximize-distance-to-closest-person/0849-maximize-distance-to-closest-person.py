d1b2ca40cfbe7b03179164c558d4a2dd33e612e6
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-20 19:15:12 +0530

    Time: 147 ms (41.58%), Space: 17.6 MB (6.96%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
@@ -4,15 +4,9 @@ class Solution:
         n = len(seats)
         for i, ch in enumerate(seats):
             if ch: res.append(i)
+        ans = res[0]
         for i in range(len(res)):
-            if i == 0:
-                ans = res[i]
-            if i == len(res) - 1:
-                ans = max(ans, n - res[i] - 1)
-                dist = res[i] - res[i - 1]
-                ans = max(ans, dist // 2)
-    
-            else:
-                dist = res[i] - res[i - 1]
-                ans = max(ans, dist // 2)
+            if i == len(res) - 1: ans = max(ans, n - res[i] - 1)
+            dist = res[i] - res[i - 1]
+            ans = max(ans, dist // 2)
         return ans
