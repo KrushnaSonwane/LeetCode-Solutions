63fb19fc1f5be496badac0b84ebcae678a35ca7b
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-21 20:17:38 +0530

    Time: 1059 ms (72.12%), Space: 100.9 MB (28.85%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,15 +1,17 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
         n = len(nums)
-        @cache
+        dp = {}
         def dfs(i):
-            if i == n: return True
-            res = False
-            if i + 1 < n and nums[i] == nums[i + 1]:
-                res = dfs(i + 2)
-            if i + 2 < n and nums[i] == nums[i + 1] == nums[i + 2]:
-                res = res or dfs(i + 3)
-            if i + 2 < n and nums[i] == nums[i + 1] - 1 == nums[i + 2] - 2:
-                res = res or dfs(i + 3)
-            return res
+            if i not in dp:
+                if i == n: return True
+                res = False
+                if i + 1 < n and nums[i] == nums[i + 1]:
+                    res = dfs(i + 2)
+                if i + 2 < n and nums[i] == nums[i + 1] == nums[i + 2]:
+                    res = res or dfs(i + 3)
+                if i + 2 < n and nums[i] == nums[i + 1] - 1 == nums[i + 2] - 2:
+                    res = res or dfs(i + 3)
+                dp[i] = res
+            return dp[i]
         return dfs(0)
