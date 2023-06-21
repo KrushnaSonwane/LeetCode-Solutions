a6d413e269b9f3b251013af37409d7ae7fd59772
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-21 19:52:09 +0530

    Time: 50 ms (42.31%), Space: 16.4 MB (25.96%) - LeetHub

---
+++ 1276-number-of-burgers-with-no-waste-of-ingredients/1276-number-of-burgers-with-no-waste-of-ingredients.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numOfBurgers(self, a: int, b: int) -> List[int]:
+        if a % 2 or b * 2 > a: return []
+        if not a and not b: return [0, 0]
+        l, r = 0, b
+        while r > l:
+            mid = (r + l) // 2
+            if mid * 4 > a:
+                r = mid
+            else:
+                t1, t2 = a - mid * 4, b - mid
+                if t2 * 2 == t1: return [mid, t2]
+                else:
+                    if t2 * 2 > t1:
+                        r = mid
+                    else:
+                        l = mid + 1
+        return []
