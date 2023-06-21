9a6b3e40a82abc738f3673315bfd8e9a7ea74b81
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-21 20:21:46 +0530

    Time: 1046 ms (76.92%), Space: 100.7 MB (29.81%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -7,17 +7,11 @@ class Solution:
                 if i == n: return True
                 res = False
                 if i + 1 < n and nums[i] == nums[i + 1]:
-                    if dfs(i + 2): 
-                        dp[i] = 1
-                        return 1
+                    res = dfs(i + 2)
                 if i + 2 < n and nums[i] == nums[i + 1] == nums[i + 2]:
-                    if dfs(i + 3): 
-                        dp[i] = 1
-                        return 1
+                    res = res or dfs(i + 3)
                 if i + 2 < n and nums[i] == nums[i + 1] - 1 == nums[i + 2] - 2:
-                    if dfs(i + 3): 
-                        dp[i] = 1
-                        return 1
-                dp[i] = False
+                    res = res or dfs(i + 3)
+                dp[i] = res
             return dp[i]
         return dfs(0)
