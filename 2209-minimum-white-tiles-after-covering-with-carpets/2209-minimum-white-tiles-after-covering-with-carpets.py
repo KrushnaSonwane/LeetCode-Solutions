c461202cc8bffc41f07ef9d4bbc9e7bed4baa86f
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-21 17:06:12 +0530

    Time: 7340 ms (14.00%), Space: 483.5 MB (8.00%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
@@ -4,11 +4,12 @@ class Solution:
         for dig in floor:
             sum_ += dig == '1'
             pre.append(sum_)
-        @cache
+        dp = {}
         def dfs(i, k):
-            if len(floor) < i or k == 0: return 0
-            res = dfs(i + 1, k)
-            curr = pre[min(len(floor), i + carpetLen - 1)] - pre[i - 1]
-            res = max(res, dfs(i + carpetLen, k - 1) + curr)
-            return res
+            if (i, k) not in dp:
+                if len(floor) < i or k == 0: return 0
+                curr = pre[min(len(floor), i + carpetLen - 1)] - pre[i - 1]
+                res = max(dfs(i + 1, k), dfs(i + carpetLen, k - 1) + curr)
+                dp[(i, k)] = res
+            return dp[(i, k)]
         return pre[-1] - dfs(1, numCarpets)
