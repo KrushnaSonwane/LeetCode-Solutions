08383a2c56dad76ac88ba881872ad83e8d2e2862
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-22 20:43:13 +0530

    Time: 89 ms (23.08%), Space: 13.5 MB (100.00%) - LeetHub

---
+++ 1156-swap-for-longest-repeated-character-substring/1156-swap-for-longest-repeated-character-substring.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxRepOpt1(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        count = collections.Counter(text)
+        hashT = defaultdict(int)
+        res = 0
+        ptr = 0
+        for ch in text:
+            hashT[ch] += 1
+            curr = 0
+            for ch2 in hashT:
+                if hashT[ch2] > 1: curr += 1
+            while len(hashT) > 2 or curr == 2:
+                hashT[text[ptr]] -= 1
+                if hashT[text[ptr]] == 1:
+                    curr -= 1
+                if hashT[text[ptr]] == 0: del hashT[text[ptr]]
+                ptr += 1                
+            for ch2 in hashT:
+                res = max(res, hashT[ch2] + (1 if count[ch2] > hashT[ch2] else 0))
+        return res
