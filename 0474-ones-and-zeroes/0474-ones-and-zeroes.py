4a1927f8fb6f62b331a8b00f0249b7d2e335e9b6
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-22 23:59:03 +0530

    Time: 2039 ms (86.33%), Space: 265.7 MB (30.28%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -1,25 +1,16 @@
-class Solution(object):
-    def findMaxForm(self, strs, m, n):
-        """
-        :type strs: List[str]
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         hashT = {}
         for i, number in enumerate(strs):
             hashT[i] = [0, 0]
             for ch in number:
                 if ch == '0': hashT[i][0] += 1
                 else: hashT[i][1] += 1
-
-        dp = {}
+        @cache
         def dfs(i, x, y):
-            if (i, x, y) not in dp:
-                if i == len(strs): return 0
-                res = dfs(i + 1, x, y)
-                if x >= hashT[i][0] and y >= hashT[i][1]:
-                    res = max(res, dfs(i + 1, x - hashT[i][0], y - hashT[i][1]) + 1)
-                dp[(i, x, y)] = res
-            return dp[(i, x, y)]
+            if i == len(strs): return 0
+            res = dfs(i + 1, x, y)
+            if x >= hashT[i][0] and y >= hashT[i][1]:
+                res = max(res, dfs(i + 1, x - hashT[i][0], y - hashT[i][1]) + 1)
+            return res
         return dfs(0, m, n)
