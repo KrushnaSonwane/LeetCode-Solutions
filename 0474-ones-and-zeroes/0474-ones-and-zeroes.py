e1c910e9af1220b6ed283419bea20a9691d6e75f
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-23 00:00:45 +0530

    Time: 2318 ms (83.31%), Space: 297.1 MB (7.53%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -1,16 +1,11 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        hashT = {}
-        for i, number in enumerate(strs):
-            hashT[i] = [0, 0]
-            for ch in number:
-                if ch == '0': hashT[i][0] += 1
-                else: hashT[i][1] += 1
         @cache
         def dfs(i, x, y):
             if i == len(strs): return 0
             res = dfs(i + 1, x, y)
-            if x >= hashT[i][0] and y >= hashT[i][1]:
-                res = max(res, dfs(i + 1, x - hashT[i][0], y - hashT[i][1]) + 1)
+            a, b = strs[i].count('0'), strs[i].count("1")
+            if x >= a and y >= b:
+                res = max(res, dfs(i + 1, x - a, y - b) + 1)
             return res
         return dfs(0, m, n)
