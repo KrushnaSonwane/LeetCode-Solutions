176ab6fedb68e574e3651f8f60c9c9de8dc99f1a
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-22 20:45:17 +0530

    Time: 95 ms (23.08%), Space: 13.5 MB (100.00%) - LeetHub

--- 1156-swap-for-longest-repeated-character-substring/1156-swap-for-longest-repeated-character-substring.py
+++ 1156-swap-for-longest-repeated-character-substring/1156-swap-for-longest-repeated-character-substring.py
@@ -6,8 +6,7 @@ class Solution(object):
         """
         count = collections.Counter(text)
         hashT = defaultdict(int)
-        res = 0
-        ptr = 0
+        res = ptr = 0
         for ch in text:
             hashT[ch] += 1
             curr = 0
@@ -15,10 +14,9 @@ class Solution(object):
                 if hashT[ch2] > 1: curr += 1
             while len(hashT) > 2 or curr == 2:
                 hashT[text[ptr]] -= 1
-                if hashT[text[ptr]] == 1:
-                    curr -= 1
+                if hashT[text[ptr]] == 1: curr -= 1
                 if hashT[text[ptr]] == 0: del hashT[text[ptr]]
                 ptr += 1                
             for ch2 in hashT:
-                res = max(res, hashT[ch2] + (1 if count[ch2] > hashT[ch2] else 0))
+                res = max(res, hashT[ch2] + int(count[ch2] > hashT[ch2]))
         return res
