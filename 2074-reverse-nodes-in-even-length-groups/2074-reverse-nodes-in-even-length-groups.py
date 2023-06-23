631ad7b0637bd80068e7d5de0ac4bf33f3402c9d
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-23 18:52:27 +0530

    Time: 2088 ms (68.70%), Space: 56.8 MB (7.32%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.py
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.py
@@ -5,30 +5,26 @@
 #         self.next = next
 class Solution:
     def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def rev(p1, p2):
+            if p2 > p1:
+                arr[p1], arr[p2] = arr[p2], arr[p1]
+                rev(p1 + 1, p2 - 1)
+        
         arr, temp = [], head
         while temp: 
             arr.append(temp.val)
             temp = temp.next
-        currSize, n = 1, len(arr)
-        i = 0
+        currSize, n, i = 1, len(arr), 0
+        
         while n >= currSize:
-            if currSize % 2:
-                i += currSize
-            else:
-                ptr1, ptr2 = i, i + currSize - 1
-                while ptr2 > ptr1:
-                    arr[ptr2], arr[ptr1] = arr[ptr1], arr[ptr2]
-                    ptr1 += 1
-                    ptr2 -= 1
-                i += currSize
+            if not currSize % 2:
+                rev(i, i + currSize - 1)
+            i += currSize
             n -= currSize
             currSize += 1
-        if not n % 2:
-            ptr1, ptr2 = i, len(arr) - 1
-            while ptr2 > ptr1:
-                arr[ptr2], arr[ptr1] = arr[ptr1], arr[ptr2]
-                ptr1 += 1
-                ptr2 -= 1
+            
+        if not n % 2: rev(i, len(arr) - 1)
+            
         temp = head
         for num in arr:
             temp.val = num
