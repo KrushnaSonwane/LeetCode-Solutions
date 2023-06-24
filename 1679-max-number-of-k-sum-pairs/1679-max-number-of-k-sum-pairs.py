8ca6a0b4c68c0c5a3acb3d53800e56ea89c296dd
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-24 19:42:47 +0530

    Time: 600 ms (66.18%), Space: 22.5 MB (87.09%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -5,15 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        res = 0
         nums.sort()
         l, r = 0, len(nums) - 1
+        res = 0
         while r > l:
-            if nums[l] + nums[r] == k:
+            sum_ = nums[l] + nums[r]
+            if sum_ == k: 
                 res += 1
-                l += 1; r -= 1
-            elif nums[l] + nums[r] > k:
-                r -= 1
-            else:
                 l += 1
+                r -= 1
+            elif sum_ > k: r -= 1
+            else: l += 1
         return res
