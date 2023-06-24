180ce2c42f54a92228c8eca99d8af291c62b74a8
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-24 22:34:54 +0530

    Time: 609 ms (50.00%), Space: 53.7 MB (75.00%) - LeetHub

--- 2746-decremental-string-concatenation/2746-decremental-string-concatenation.py
+++ 2746-decremental-string-concatenation/2746-decremental-string-concatenation.py
@@ -5,7 +5,6 @@ class Solution:
             if i == len(words): return 0
             res = float("inf")
             s = words[i]
-            
             if f == s[-1]:
                 res = min(res, (len(s) - 1) + dfs(i + 1, s[0], l))
             if l == s[0]:
