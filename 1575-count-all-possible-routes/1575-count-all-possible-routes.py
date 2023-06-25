c51c9a86f1c609822cfd4037b91071241d07e383
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-25 10:25:59 +0530

    Time: 1966 ms (75.94%), Space: 22.9 MB (50.38%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.py
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.py
@@ -1,22 +1,12 @@
-class Solution(object):
-    def countRoutes(self, locations, start, finish, fuel):
-        """
-        :type locations: List[int]
-        :type start: int
-        :type finish: int
-        :type fuel: int
-        :rtype: int
-        """
-        dp = [[-1 for _ in range(fuel + 1)] for _ in locations]
-        def dfs(i, f):
-            if 0 > f: return 0
-            if dp[i][f] != -1: return dp[i][f]
-            count = 1 if i == finish else 0
-            for j in range(len(locations)):
-                if i == j: continue
-                diff = abs(locations[i] - locations[j])
-                if diff <= f:
-                    count += dfs(j, f - diff)
-            dp[i][f] = count
-            return count
-        return dfs(start, fuel) % (10**9 + 7)
+class Solution:
+    def countRoutes(self, A: List[int], start: int, finish: int, fuel: int) -> int:
+        MOD = 10 ** 9 + 7
+        @cache
+        def dfs(i, fuel):
+            res = 0
+            if i == finish: res += 1
+            for j in range(len(A)):
+                if i != j and fuel >= abs(A[i] - A[j]):
+                    res += dfs(j, fuel - abs(A[i] - A[j])) 
+            return res % MOD
+        return dfs(start, fuel)
