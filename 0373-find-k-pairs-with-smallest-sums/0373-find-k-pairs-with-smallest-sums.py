d00ca51e539b0bffa53f73328fbb188769aeeafe
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-27 12:24:21 +0530

    Time: 1034 ms (58.43%), Space: 30.9 MB (34.34%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -1,23 +1,19 @@
 class Solution(object):
-    def kSmallestPairs(self, nums1, nums2, k):
+    def kSmallestPairs(self, n1, n2, k):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :type k: int
         :rtype: List[List[int]]
         """
-        res, visit = [], set({(0, 0)})
-        n1, n2 = len(nums1), len(nums2)
-        queue = [[nums1[0] + nums2[0], 0, 0]]
-        heapq.heapify(queue)
-        while k and queue:
-            _, i, j = heapq.heappop(queue)
-            res.append([nums1[i], nums2[j]])
-            if i + 1 < n1 and (i + 1, j) not in visit:
-                visit.add((i + 1, j))
-                heapq.heappush(queue, [nums1[i + 1] + nums2[j], i + 1, j])
-            if j + 1 < n2 and (i, j + 1) not in visit:
-                visit.add((i, j + 1))
-                heapq.heappush(queue, [nums1[i] + nums2[j + 1], i, j + 1])
+        m, n, res, A = len(n1), len(n2), [], [[n1[0] + n2[0], 0,0]]
+        visit = {(0, 0)}
+        while A and k:
+            _, i, j = heappop(A)
+            res.append([n1[i], n2[j]])
+            for x, y in [[i + 1, j], [i, j + 1]]:
+                if x < m and y < n and (x, y) not in visit:
+                    visit.add((x, y))
+                    heappush(A, [n1[x]+n2[y], x, y])
             k -= 1
         return res
