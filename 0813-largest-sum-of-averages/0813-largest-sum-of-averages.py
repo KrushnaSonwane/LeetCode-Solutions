f4b5dc26223dc1822c7530e6a5140cda156d8f77
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-28 20:34:32 +0530

    Time: 155 ms (92.63%), Space: 17.5 MB (42.11%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.py
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.py
@@ -1,18 +1,18 @@
 class Solution:
-    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
+    def largestSumOfAverages(self, A: List[int], k: int) -> float:
         dp = {}
-        for i in range(1, len(nums)): nums[i] += nums[i - 1]
+        for i in range(1, len(A)): A[i] += A[i - 1]
         def dfs(i, k):
             if (i, k) not in dp:
-                if i == len(nums): return 0
+                if i == len(A): return 0
                 if k == 1:
-                    sum_ = nums[-1] if i == 0 else nums[-1] - nums[i - 1]
-                    size = len(nums) - i
+                    sum_ = A[-1] if i == 0 else A[-1] - A[i - 1]
+                    size = len(A) - i
                     return sum_ / size
                 res = 0
-                for j in range(i, len(nums) - (k - 1)):
-                    sum_, size = nums[j] if i == 0 else nums[j] - nums[i - 1], (j - i + 1)
-                    res = max(res, (sum_ / size) + dfs(j + 1, k - 1))
+                for j in range(i, len(A) - (k - 1)):
+                    sum_, size = A[j] if i == 0 else A[j] - A[i - 1], (j - i + 1)
+                    res = max(res, sum_ / size + dfs(j + 1, k - 1))
                 dp[(i, k)] = res
             return dp[(i, k)]
         return dfs(0, k)
