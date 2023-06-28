a5a904acafa7a77af4abca48e4d3bbb89dc3c455
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-06-28 16:24:01 +0530

    Time: 1754 ms (65.88%), Space: 78.9 MB (85.88%) - LeetHub

---
+++ 2747-count-zero-request-servers/2747-count-zero-request-servers.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countServers(self, n: int, logs: List[List[int]], x: int, queries: List[int]) -> List[int]:
+        logs.sort(key = lambda x: (x[1]))
+        Q = [[val, i] for i, val in enumerate(queries)]
+        Q.sort()
+        res = [0] * len(Q)
+        count, ptr1, ptr2 = 0, 0, 0
+        hashT = defaultdict(int)
+        for key, i in Q:
+            while len(logs) > ptr2 and logs[ptr2][1] <= key:
+                hashT[logs[ptr2][0]] += 1
+                if hashT[logs[ptr2][0]] == 1: count += 1
+                ptr2 += 1
+            while len(logs) > ptr1 and key - x > logs[ptr1][1]:
+                hashT[logs[ptr1][0]] -= 1
+                if hashT[logs[ptr1][0]] == 0: count -= 1
+                ptr1 += 1
+            res[i] = n - count
+        return res
