a0d30fc40fff47872485a7f49af0686ccd0b1de2
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-02 23:50:49 +0530

    Time: 1206 ms (69.13%), Space: 18.1 MB (81.48%) - LeetHub

---
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findLexSmallestString(self, s: str, a: int, b: int) -> str:
+        res = s
+        visit = set({s})
+        Q = [s]
+        while Q:
+            currS = Q.pop(0)
+            res = min(res, currS)
+            A = []
+            for ch in range(1, len(currS), 2):
+                A.append(currS[ch - 1])
+                A.append(str(((int(currS[ch]) + a) % 10)))
+            A = ''.join(A)
+            if A not in visit:
+                Q.append(A)
+                visit.add(A)
+            A = currS[-b: len(currS)] + currS[:-b]
+            if A not in visit:
+                visit.add(A)
+                Q.append(A)
+        return res 
