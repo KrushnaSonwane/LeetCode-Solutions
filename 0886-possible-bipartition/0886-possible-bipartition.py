1a81ab9ddc06fc06483becb03740cca105f35c2e
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-04 01:41:06 +0530

    Time: 697 ms (95.56%), Space: 21.9 MB (90.00%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -5,18 +5,17 @@ class Solution:
         for a, b in dislikes:
             adj[a].append(b)
             adj[b].append(a)
-
-        def dfs(node, C):
-            color[node] = C
-            res = True
-            for child in adj[node]:
-                if child not in color:
-                    res = res and dfs(child, 'g' if C == 'r' else 'r')
-                else:
-                    if color[child] == C: return False
-            return res
         
-        for node in range(n):
-            if node not in color:
-                if not dfs(node, 'r'): return False
+        for num in range(n):
+            if num not in color:
+                Q = [[num, 0]]
+                while Q:
+                    node, c = Q.pop(0)
+                    color[node] = c
+                    for child in adj[node]:
+                        if child not in color:
+                            Q.append([child, not c])
+                            color[child] = not c
+                        else:
+                            if color[child] == c: return False
         return True
