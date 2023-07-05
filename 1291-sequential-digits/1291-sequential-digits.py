129b93456f2c4d433795c9bea13dc23bc74c602f
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-05 19:02:29 +0530

    Time: 51 ms (25.98%), Space: 16.3 MB (75.98%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        res = []
+        def dfs(num):
+            nonlocal res
+            if num > high: return
+            if low <= num and high >= num: res.append(num)
+            if str(num)[-1] == '9': return
+            num = str(num) + str(int(str(num)[-1]) + 1)
+            dfs(int(num))
+        for i in range(1, 9):
+            dfs(i)
+        return sorted(res)
