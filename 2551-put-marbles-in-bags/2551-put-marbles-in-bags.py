3879e355c1f0366ed0a9603d4a1d9cb3ac8839cd
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-08 11:31:40 +0530

    Time: 712 ms (89.38%), Space: 30 MB (55.75%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -1,5 +1,5 @@
 class Solution:
     def putMarbles(self, W: List[int], k: int) -> int:
-        A = [W[i]+W[i+1] for i in range(len(W)-1)]
+        A = [W[i] + W[i+1] for i in range(len(W) - 1)]
         A.sort()
         return sum(A[-1:-k:-1]) - sum(A[:k-1])
