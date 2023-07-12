d4913ba35bd41d7c1cb0e2f2f6b1e8a28e549f5c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-13 00:15:19 +0530

    Added solution - LeetHub

---
+++ Unique Number of Occurrences - GFG/unique-number-of-occurrences.py
@@ -0,0 +1,45 @@
+
+from typing import List
+import collections
+
+class Solution:
+    def isFrequencyUnique(self, n : int, arr : List[int]) -> bool:
+        hashT = collections.Counter(arr)
+        A = set()
+        for val in hashT.values():
+            if val in A: return 0
+            A.add(val)
+        return 1
+
+
+#{ 
+ # Driver Code Starts
+
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(n)
+        
+        obj = Solution()
+        res = obj.isFrequencyUnique(n, arr)
+        
+        result_val = 1 if res else 0
+        print(result_val)
+
+# } Driver Code Ends
