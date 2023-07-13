ad3d8c0a7bbf1509eca0d463e3aa9976776907e4
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-13 14:21:29 +0530

    Time: 3582 ms (17.11%), Space: 649 MB (22.99%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -5,8 +5,6 @@ class Solution:
         def dfs(pos, k):
             if (pos, k) not in dp:
                 if k == 0: return int(pos == endPos)
-                # if pos < 0: return 0
-                res = dfs(pos-1, k-1) + dfs(pos+1, k-1)
-                dp[(pos, k)] = res
+                dp[(pos, k)] = (dfs(pos-1, k-1) + dfs(pos+1, k-1)) % mod
             return dp[(pos, k)]
-        return dfs(startPos, A) % mod
+        return dfs(startPos, A) 
