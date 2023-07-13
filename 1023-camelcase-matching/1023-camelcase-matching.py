1ec90c0a021ff212f94e469309584e38dd968902
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-13 17:03:07 +0530

    Time: 46 ms (71.11%), Space: 16.3 MB (53.33%) - LeetHub

---
+++ 1023-camelcase-matching/1023-camelcase-matching.py
@@ -0,0 +1,16 @@
+class Solution:
+    def camelMatch(self, Q: List[str], P: str) -> List[bool]:
+        N, res = len(Q), []
+        for word in Q:
+            i = 0
+            for ch in word:
+                if ch == ch.upper():
+                    if len(P) > i and P[i] == ch:
+                        i += 1
+                    else: 
+                        res.append(False)
+                        break
+                elif len(P) > i and P[i] == ch:
+                    i += 1
+            else: res.append(i == len(P))
+        return res
