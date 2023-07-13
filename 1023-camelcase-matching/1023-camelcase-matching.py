d1ff4b392aa40047251d2bc40852d7ea0c664bfe
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-13 17:06:21 +0530

    Time: 43 ms (85.00%), Space: 16.4 MB (53.33%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.py
+++ 1023-camelcase-matching/1023-camelcase-matching.py
@@ -5,12 +5,10 @@ class Solution:
             i = 0
             for ch in word:
                 if ch == ch.upper():
-                    if len(P) > i and P[i] == ch:
-                        i += 1
+                    if len(P) > i and P[i] == ch: i += 1
                     else: 
                         res.append(False)
                         break
-                elif len(P) > i and P[i] == ch:
-                    i += 1
+                elif len(P) > i and P[i] == ch: i += 1
             else: res.append(i == len(P))
         return res
