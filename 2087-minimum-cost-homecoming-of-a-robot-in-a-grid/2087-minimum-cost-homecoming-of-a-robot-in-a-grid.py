4abb1a8b667b51732744c98a6e6219c5183b20b9
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-14 17:14:53 +0530

    Time: 1325 ms (100.00%), Space: 31.2 MB (61.47%) - LeetHub

--- 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.py
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.py
@@ -1,12 +1,8 @@
 class Solution:
-    def minCost(self, A: List[int], B: List[int], rowCosts: List[int], colCosts: List[int]) -> int:
+    def minCost(self, A: List[int], B: List[int], AA: List[int], BB: List[int]) -> int:
         res = 0
-        if A[0] > B[0]:
-            res += sum(rowCosts[B[0]:A[0]])
-        else:
-            res += sum(rowCosts[A[0]+1: B[0]+1])
-        if A[1] > B[1]:
-            res += sum(colCosts[B[1]:A[1]])
-        else:
-            res += sum(colCosts[A[1]+1: B[1]+1])
+        if A[0] > B[0]: res += sum(AA[B[0]:A[0]])
+        else: res += sum(AA[A[0]+1: B[0]+1])
+        if A[1] > B[1]: res += sum(BB[B[1]:A[1]])
+        else: res += sum(BB[A[1]+1: B[1]+1])
         return res
