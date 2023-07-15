019eccb4bda4dc496ed65ac1ca63124f0d4afd3c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-15 21:18:49 +0530

    Time: 97 ms (59.28%), Space: 18 MB (26.70%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.py
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -1,15 +1,12 @@
 class Solution:
     def largestPalindromic(self, num: str) -> str:
         hashT = collections.Counter(num)
-        A, B = {}, []
+        A, B, res = {}, [], []
         for n in hashT:
             if hashT[n]%2 == 0: A[n] = hashT[n]
-            else:
-                if hashT[n] > 1: A[n] = hashT[n]-1
-                B.append(n)
-        res = []
-        for digit in '9876543210':
-            if digit in A:
-                for _ in range(A[digit]//2): res.append(digit)
+            elif hashT[n] > 1: A[n] = hashT[n]-1
+            if hashT[n] % 2: B.append(n)
+        for digit in sorted(A.keys())[::-1]:
+            for _ in range(A[digit]//2): res.append(digit)
         if ('0' in res and res.count('0') == len(res)) or not res: return max(B) if B else '0'
         return ''.join(res) + (max(B) if B else '') + ''.join(res[::-1])
