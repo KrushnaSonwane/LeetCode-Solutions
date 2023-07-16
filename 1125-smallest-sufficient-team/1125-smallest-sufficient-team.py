81d97849fee92b4c41d2ba8b856020f2ae14f97d
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-16 20:13:13 +0530

    Time: 645 ms (33.55%), Space: 18.9 MB (60.13%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
@@ -9,17 +9,22 @@ class Solution:
         for i in range(n):
             for skill in people[i]:
                 skills_mask_of_person[i] |= 1 << skill_id[skill]
-        dp = [(1 << n) - 1] * (1 << m)
+        dp = [-1] * (1 << m)
         dp[0] = 0
-        for skills_mask in range(1, 1 << m):
-            for i in range(n):
-                smaller_skills_mask = skills_mask & ~skills_mask_of_person[i]
-                if smaller_skills_mask != skills_mask:
-                    people_mask = dp[smaller_skills_mask] | (1 << i)
-                    if people_mask.bit_count() < dp[skills_mask].bit_count():
-                        dp[skills_mask] = people_mask
 
-        answer_mask = dp[(1 << m) - 1]
+        def f(skills_mask):
+            if dp[skills_mask] != -1:
+                return dp[skills_mask]
+            for i in range(n):
+                new_skills_mask = skills_mask & ~skills_mask_of_person[i]
+                if new_skills_mask != skills_mask:
+                    people_mask = f(new_skills_mask) | (1 << i)
+                    if (dp[skills_mask] == -1 or
+                        people_mask.bit_count()
+                       < dp[skills_mask].bit_count()):
+                        dp[skills_mask] = people_mask
+            return dp[skills_mask]
+        answer_mask = f((1 << m) - 1)
         ans = []
         for i in range(n):
             if (answer_mask >> i) & 1:
