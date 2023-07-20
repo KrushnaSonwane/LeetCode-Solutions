b8805f6213662c199d2ce783d7ec054709952a9c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-20 20:24:27 +0530

    Time: 1582 ms (9.67%), Space: 207.9 MB (18.46%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -2,10 +2,12 @@ class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         def getSum(r, s):
             return sum(ord(s[k]) for k in range(r, len(s)))
-        @cache
+        dp = {}
         def dfs(i, j):
-            if i == len(s1): return getSum(j, s2)
-            if j == len(s2): return getSum(i, s1)
-            if s1[i] == s2[j]: return dfs(i+1,j+1)
-            return min(dfs(i+1,j) + ord(s1[i]), dfs(i,j+1) + ord(s2[j]))
+            if (i, j) not in dp:
+                if i == len(s1): return getSum(j, s2)
+                if j == len(s2): return getSum(i, s1)
+                if s1[i] == s2[j]: dp[(i,j)] = dfs(i+1,j+1)
+                else: dp[(i,j)] = min(dfs(i+1,j) + ord(s1[i]), dfs(i,j+1) + ord(s2[j]))
+            return dp[(i, j)]
         return dfs(0,0)
