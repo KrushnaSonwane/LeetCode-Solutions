78022f9fe83fbc97ff26317fc91561df24f89637
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-20 14:09:37 +0530

    Time: 117 ms (34.43%), Space: 17.6 MB (57.73%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -4,17 +4,12 @@ class Solution:
         for val in asteroids:
             if val > 0: stack.append(val)
             else:
-                if not stack:
-                    stack.append(val)
-                elif stack and stack[-1] < 0:
-                    stack.append(val)
+                if not stack: stack.append(val)
+                elif stack and stack[-1] < 0: stack.append(val)
                 else:
-                    while stack and stack[-1] > 0 and stack[-1] < abs(val):
-                        stack.pop()
-                    if not stack:
-                        stack.append(val)
-                    elif stack and stack[-1] > 0 and stack[-1] == abs(val):
-                        stack.pop()
+                    while stack and stack[-1] > 0 and stack[-1] < abs(val): stack.pop()
+                    if not stack: stack.append(val)
+                    elif stack and stack[-1] > 0 and stack[-1] == abs(val): stack.pop()
                     elif stack and stack[-1] > abs(val): continue
                     else: stack.append(val)
         return stack
