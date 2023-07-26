f9c8ad9f0527865bd963faae4acaf9e2e244654b
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-27 00:15:42 +0530

    Time: 337 ms (83.15%), Space: 16.5 MB (52.81%) - LeetHub

--- 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.py
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.py
@@ -1,13 +1,13 @@
 class Solution:
-    def numTriplets(self, nums1: List[int], nums2: List[int]) -> int:
-        def solve(nums1, nums2):
+    def numTriplets(self, A: List[int], B: List[int]) -> int:
+        def solve(A, B):
             hashT = defaultdict(int)
-            for num in nums1:
+            for num in A:
                 hashT[num**2]+=1
             res = 0
-            for i in range(len(nums2)):
-                for j in range(i+1, len(nums2)):
-                    curr = nums2[i]*nums2[j]
+            for i in range(len(B)):
+                for j in range(i+1, len(B)):
+                    curr = B[i]*B[j]
                     if curr in hashT: res += hashT[curr]
             return res
-        return solve(nums1, nums2) + solve(nums2, nums1)
+        return solve(A, B) + solve(B, A)
