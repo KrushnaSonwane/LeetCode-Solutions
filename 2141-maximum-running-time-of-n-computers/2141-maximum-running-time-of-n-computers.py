ecafaeb0185a21051426b6937a3942063ef27433
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-27 16:42:58 +0530

    Time: 1844 ms (46.99%), Space: 30.7 MB (28.92%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
@@ -1,19 +1,21 @@
 class Solution:
     def maxRunTime(self, n: int, B: List[int]) -> int:
         B.sort()
-        def check(mid):
+        
+        def check(x):
             sum_, count = 0, 0
             for num in B:
                 sum_ += num
-                if sum_ >= mid:
-                    sum_ -= mid
+                if sum_ >= x:
+                    sum_ -= x
                     count += 1
             return count >= n
+        
         l, r = 1, sum(B)+1
+        
         while r > l:
-            mid = (r+l)//2
-            if check(mid):
-                l = mid + 1
-            else:
-                r = mid
+            x = (r+l)//2
+            if check(x): l = x + 1
+            else: r = x
+                
         return l-1
