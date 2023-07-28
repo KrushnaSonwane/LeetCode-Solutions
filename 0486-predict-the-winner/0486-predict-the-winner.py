db483312c3964479f4a22ed8bb70e308e67ad7a4
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-28 13:32:37 +0530

    Time: 41 ms (96.36%), Space: 16.8 MB (7.89%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -3,9 +3,6 @@ class Solution:
         @cache
         def dfs(i, j, alice):
             if i > j: return 0
-            if alice:
-                res = max(nums[j]+dfs(i,j-1,False), nums[i]+dfs(i+1,j,False))
-            else:
-                res = min(-nums[j]+dfs(i,j-1,True), -nums[i]+dfs(i+1,j,True))
-            return res
-        return dfs(0,len(nums)-1,True) >= 0
+            if alice: return max(nums[j]+dfs(i,j-1,False), nums[i]+dfs(i+1,j,False))
+            return min(-nums[j]+dfs(i,j-1,True), -nums[i]+dfs(i+1,j,True))
+        return dfs(0,len(nums)-1,True) > -1
