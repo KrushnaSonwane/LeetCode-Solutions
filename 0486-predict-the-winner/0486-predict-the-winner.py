25488c3fbac95dd580d6b35f51bda182b3dd6241
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-28 13:35:08 +0530

    Time: 37 ms (98.42%), Space: 16.6 MB (31.92%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,5 +1,6 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
+        @cache
         def dfs(i, j, alice):
             if i > j: return 0
             if alice: return max(nums[j]+dfs(i,j-1,False), nums[i]+dfs(i+1,j,False))
