2d537553daea8ee013421ca48a64cc19a2c3c8cc
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-07-29 13:21:19 +0530

    Time: 39 ms (99.53%), Space: 16.8 MB (91.98%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -2,10 +2,10 @@ class Solution:
     def soupServings(self, N: int) -> float:
         if N > 4800: return 1
         @cache
-        def f(a, b):
+        def dfs(a, b):
             if a <= 0 and b <= 0: return 0.5
             if a <= 0: return 1
             if b <= 0: return 0
-            return 0.25 * (f(a - 4, b) + f(a - 3, b - 1) + f(a - 2, b - 2) + f(a - 1, b - 3))
+            return 0.25 * (dfs(a - 4, b) + dfs(a - 3, b - 1) + dfs(a - 2, b - 2) + dfs(a - 1, b - 3))
         N = math.ceil(N / 25.0)
-        return f(N, N)
+        return dfs(N, N)
