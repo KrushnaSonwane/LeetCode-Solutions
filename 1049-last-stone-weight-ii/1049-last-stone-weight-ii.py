a5b033523b00f358b52ac79dffe8cddedf2abd5a
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-02 17:50:36 +0530

    Time: 79 ms (68.35%), Space: 25.8 MB (5.04%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -3,6 +3,5 @@ class Solution:
         @cache
         def dfs(i, f, s):
             if i == len(A): return abs(f-s)
-            res = min(dfs(i+1, f+A[i], s), dfs(i+1, f, s+A[i]))
-            return res
+            return min(dfs(i+1, f+A[i], s), dfs(i+1, f, s+A[i]))
         return dfs(0, 0, 0)
