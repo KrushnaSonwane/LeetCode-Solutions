867209a39073707c50e91852be0961f3aca18223
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-04 20:13:15 +0530

    Time: 289 ms (73.38%), Space: 16.8 MB (37.27%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,19 +1,11 @@
-class Solution(object):
-    def minExtraChar(self, s, dictionary):
-        """
-        :type s: str
-        :type dictionary: List[str]
-        :rtype: int
-        """
-        hashT = {word for word in dictionary}
-        dp = [-1 for _ in s]
-        def dfs(i, size):
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        @cache
+        def dfs(i):
             if i == len(s): return 0
-            if dp[i] != -1: return dp[i]
-            min_ = dfs(i + 1, size) + 1
-            for j in range(i, len(s)):
-                if s[i:j + 1] in hashT:
-                    min_ = min(min_, dfs(j + 1, size - (j - i) + 1))
-            dp[i] = min_
-            return dp[i]
-        return dfs(0, len(s))
+            res = dfs(i+1) + 1
+            for w in dictionary:
+                if i+len(w) <= len(s) and s[i: i+len(w)]==w:
+                    res = min(res, dfs(i+len(w)))
+            return res
+        return dfs(0)
