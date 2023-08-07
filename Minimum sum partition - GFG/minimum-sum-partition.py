69ed3a8f0817d52af5219f8394e47cb571ebeac0
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-07 18:15:10 +0530

    Added solution - LeetHub

---
+++ Minimum sum partition - GFG/minimum-sum-partition.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+class Solution:
+	def minDifference(self, arr, n):
+		dp = {}
+		def dfs(i, sum_):
+		    if (i, sum_) not in dp:
+    		    if i == n: return abs(sum_)
+    		    res = dfs(i+1, sum_+arr[i])
+    		    res = min(res, dfs(i+1, sum_-arr[i]))
+    		    dp[(i,sum_)] = res
+		    return dp[(i,sum_)]
+		return dfs(0, 0)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		N = int(input())
+		arr = [int(x) for x in input().split()]
+		ob = Solution()
+		ans = ob.minDifference(arr, N)
+		print(ans)
+
+# } Driver Code Ends
