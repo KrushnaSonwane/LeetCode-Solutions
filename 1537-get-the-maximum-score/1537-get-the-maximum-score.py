c686efa55d5e0840fb4840c17eada207c50bf362
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-10 01:03:58 +0530

    Time: 513 ms (89.86%), Space: 27.6 MB (65.54%) - LeetHub

--- 1537-get-the-maximum-score/1537-get-the-maximum-score.py
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.py
@@ -1,18 +1,23 @@
 class Solution:
-    def maxSum(self, N1: List[int], N2: List[int]) -> int:
-        A, B = {val: i for i, val in enumerate(N1)}, {val: i for i, val in enumerate(N2)}
-        mod = 10**9+7
-        @cache
-        def dfs(i, f):
-            if f and len(N1) == i: return 0
-            if not f and len(N2)==i: return 0
-            if f:
-                res = N1[i] + dfs(i+1, f)
-                if N1[i] in B:
-                    res = max(res, N1[i] + dfs(B[N1[i]] + 1, False))
+    def maxSum(self, A: List[int], B: List[int]) -> int:
+        res, mod = 0, 10**9+7
+        i, j = 0, 0
+        a, b = 0, 0
+        while i < len(A) or j < len(B):
+            if len(A)>i:
+                a += A[i]
+                while j < len(B) and A[i] > B[j]:
+                    b += B[j]
+                    j += 1
+                if j < len(B) and A[i]==B[j]:
+                    b += B[j]
+                    res += max(a, b)
+                    a, b = 0, 0
+                    j += 1
+                i += 1
             else:
-                res = N2[i] + dfs(i+1, f)
-                if N2[i] in A:
-                    res = max(res, N2[i] + dfs(A[N2[i]] + 1, True))
-            return res
-        return max(dfs(0, False), dfs(0, True)) % mod
+                while j < len(B):
+                    b += B[j]
+                    j += 1
+        res += max(a, b)
+        return res % mod
