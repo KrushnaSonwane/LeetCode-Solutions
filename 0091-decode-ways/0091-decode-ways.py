88d4a58a69e9728951e01393f0b20f3d09bdf50e
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-13 18:46:11 +0530

    Time: 45 ms (62.09%), Space: 16.7 MB (11.24%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,15 +1,13 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        hashT = {}
-        for i in range(97, 123):
-            hashT[(str(i-96))] = chr(i)
-        @cache
+        hashT = {str(i) for i in range(1, 27)}
+        @lru_cache(None)
         def dfs(i):
             if i == len(s): return 1
             res = 0
             if s[i] in hashT:
-                res += dfs(i+1)
-            if i+1 < len(s) and s[i:i+2] in hashT:
-                res += dfs(i+2)
+                res += dfs(i + 1)
+            if i + 1 < len(s) and s[i: i + 2] in hashT:
+                res += dfs(i + 2)
             return res
         return dfs(0)
