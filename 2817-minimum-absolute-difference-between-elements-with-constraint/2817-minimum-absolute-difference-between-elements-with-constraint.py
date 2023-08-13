341cfecaf98a8112092f107e902c366b61cb7791
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-13 09:38:41 +0530

    Time: 4068 ms (8.33%), Space: 31.9 MB (100.00%) - LeetHub

---
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
@@ -0,0 +1,16 @@
+from sortedcontainers import SortedList
+class Solution:
+    def minAbsoluteDifference(self, nums: List[int], x: int) -> int:
+        A = SortedList()
+        print()
+        res = inf
+        for i in range(x, len(nums)):
+            A.add(nums[i-x])
+            ind = bisect.bisect(A, nums[i])
+            if ind-1 < len(A):
+                res = min(res, abs(A[ind-1]-nums[i]))
+            if ind+1<len(A):
+                res = min(res, abs(A[ind+1]-nums[i]))
+            if ind < len(A):
+                res = min(res, abs(A[ind]-nums[i]))
+        return res
