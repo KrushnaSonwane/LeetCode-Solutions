4b39a1b1aa36c6333a313db6b9797ee80e01ddf6
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-14 15:37:14 +0530

    Time: 40 ms (83.02%), Space: 16.4 MB (68.55%) - LeetHub

---
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def btreeGameWinningMove(self, root: Optional[TreeNode], n: int, x: int) -> bool:
+        self.found = None
+        def dfs(root):
+            if not root: return 
+            if root.val==x: self.found = root
+            dfs(root.left)
+            dfs(root.right)
+        
+        def getSize(root):
+            if not root: return 0
+            return getSize(root.left) + getSize(root.right) + 1
+
+        dfs(root)
+        l = getSize(self.found.left)
+        r = getSize(self.found.right)
+
+        rem = n - (l+r+1)
+
+        return r > l+rem+1 or l > rem+1+r or rem > l+r+1
