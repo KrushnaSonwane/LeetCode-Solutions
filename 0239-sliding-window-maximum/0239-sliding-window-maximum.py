a55f3634906814dd00b696d88b105cdd40745c88
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-17 01:25:13 +0530

    Time: 1643 ms (54.71%), Space: 42.7 MB (5.24%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,12 +1,10 @@
 class Solution:
-    def maxSlidingWindow(self, A: List[int], k: int) -> List[int]:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         Q, res = [], []
-        for i, num in enumerate(A):
-            while Q and A[Q[-1]] <= num:
-                Q.pop()
-            Q.append(i)
-            if i-k == Q[0]: 
-                Q.pop(0)
+        for i, num in enumerate(nums):
+            heappush(Q, [-num, i])
+            while Q[0][1] <= i-k:
+                heappop(Q)
             if i+1 >= k:
-                res.append(A[Q[0]])
+                res.append(-Q[0][0])
         return res
