cfb1485dabff4586c178fb32d7ad1a584994e256
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-21 14:11:09 +0530

    Time: 775 ms (95.83%), Space: 30.7 MB (65.48%) - LeetHub

--- 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.py
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.py
@@ -2,7 +2,12 @@ class Solution:
     def getStrongest(self, A: List[int], k: int) -> List[int]:
         A.sort()
         res, m = [], A[(len(A)-1)//2]
-        for a in A:
-            res.append([abs(a-m), a])
-        res.sort(key = lambda x: (-x[0], -x[1]))
-        return [res[i][1] for i in range(k)]
+        l, r = 0, len(A)-1
+        for _ in range(k):
+            ll, rr = abs(A[l] - m), abs(A[r]-m)
+            res.append(A[l] if ll > rr else A[r])
+            if ll > rr:
+                l += 1
+            else:
+                r -= 1
+        return res
