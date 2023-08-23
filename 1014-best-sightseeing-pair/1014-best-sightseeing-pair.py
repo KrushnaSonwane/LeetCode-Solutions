16da82dc4f2ddddf55b1df82c3183d1422252ad8
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-23 13:48:54 +0530

    Time: 804 ms (5.19%), Space: 209.1 MB (5.74%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxScoreSightseeingPair(self, nums: List[int]) -> int:
         @lru_cache(None)
-        def dfs(i, taken, used):
-            if taken and used: return 0
+        def dfs(i, f, ff):
+            if f and ff: return 0
             if i >= len(nums): return -inf
-            res = dfs(i+1, taken, used)
-            if not taken:
+            res = dfs(i+1, f, ff)
+            if not f:
                 res = max(res, nums[i]+i + dfs(i+1, True, False))
             else:
                 res = max(res, nums[i]-i + dfs(i+1, True, True))
