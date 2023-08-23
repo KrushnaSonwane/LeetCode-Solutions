368b4d8afff64d15c8ffa00af649fe0ec039029a
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-23 11:53:34 +0530

    Time: 43 ms (71.50%), Space: 16.4 MB (46.15%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,20 +1,13 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        hashT, res = Counter(s), [""]
-        Q = []
-        for ch in hashT: heappush(Q, [-hashT[ch], ch])
-        while len(Q)>1:
-            a, c1 = heappop(Q)
-            b, c2 = heappop(Q)
-            if res[-1]==c1:
-                c1, c2 = c2, c1
-                a, b = b, a
-            res.append(c1)
-            res.append(c2)
-            if a+1 < 0: heappush(Q, [a+1, c1])
-            if b+1 < 0: heappush(Q, [b+1, c2])
-        if Q:
-            a, c = heappop(Q)
-            if a != -1: return ''
-            res.append(c)
+        hashT, res = Counter(s), ['']*len(s)
+        A = sorted([[hashT[ch], ch] for ch in hashT])[::-1]
+        if ceil(len(s)/2) < A[0][0]: return ''
+        i = 0
+        for count, ch in A:
+            for _ in range(count):
+                res[i] = ch
+                i += 2
+                if i >= len(s):
+                    i = 1
         return ''.join(res)
