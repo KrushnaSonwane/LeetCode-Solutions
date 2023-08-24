90757bc907a5889dd1c50fb5b04e667acb3308de
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-24 16:51:18 +0530

    Time: 746 ms (32.68%), Space: 63 MB (10.78%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -2,14 +2,14 @@ class Solution:
     def maximalSquare(self, A: List[List[str]]) -> int:
 
         @lru_cache(None)
-        def dfs(x, y):
+        def dp(x, y):
             if x in [-1, m] or y in [-1, n] or A[i][j]=='0': return 0
-            res = 1 + min(dfs(i-1, j), dfs(i, j-1), dfs(i-1, j-1))
+            res = 1 + min(dp(i-1, j), dp(i, j-1), dp(i-1, j-1))
             return res
 
         m, n = len(A), len(A[0])
         res = 0
         for i in range(m):
             for j in range(n):
-                res = max(res, dfs(i, j))
+                res = max(res, dp(i, j))
         return res*res
