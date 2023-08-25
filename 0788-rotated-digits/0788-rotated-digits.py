01e1b34d49f548439f7a3b8896ddbd4188935264
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-25 13:35:38 +0530

    Time: 58 ms (87.79%), Space: 16.3 MB (33.53%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -6,7 +6,4 @@ class Solution:
             for d in ['2', '5', '6', '9']:
                 if d in s: return 1
             return 0
-        res = 0
-        for num in range(1, n+1):
-            res += dfs(str(num))
-        return res
+        return sum(dfs(str(i)) for i in range(1, n+1))
