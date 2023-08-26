a4acb3b021acdabaf1be50c8ae2f2192abb3f37c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-26 17:53:03 +0530

    Time: 66 ms (77.46%), Space: 16.3 MB (88.81%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -3,16 +3,14 @@ class Solution:
         hashT = Counter(s)
         res, A = [], sorted([[ch, hashT[ch]] for ch in hashT])
         while len(res) != len(s):
-            i = 0
-            while len(A) > i:
-                if A[i][1] > 0:
-                    res.append(A[i][0])
+            for i, (ch, count) in enumerate(A):
+                if count > 0:
+                    res.append(ch)
                 A[i][1] -= 1
-                i += 1
-            i -= 1
-            while i > -1:
-                if A[i][1] > 0:
-                    res.append(A[i][0])
+            A = A[::-1]
+            for i, (ch, count) in enumerate(A):
+                if count > 0:
+                    res.append(ch)
                 A[i][1] -= 1
-                i -= 1
+            A = A[::-1]
         return ''.join(res)
