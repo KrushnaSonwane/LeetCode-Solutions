e4a26a7136292c885941c45392f5c604b541a87c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-26 17:48:55 +0530

    Time: 61 ms (86.64%), Space: 16.3 MB (88.81%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -1,17 +1,18 @@
 class Solution:
     def sortString(self, s: str) -> str:
         hashT = Counter(s)
-        res, Q = [], []
-        for ch in hashT:
-            heappush(Q, [ord(ch), hashT[ch]])
-        f = True
-        while Q:
-            t = []
-            for _ in range(len(Q)):
-                ch, count = heappop(Q)
-                res.append(chr(abs(ch)))
-                count -= 1
-                if count != 0: heappush(t, [-ch if f else abs(ch), count])
-            Q = t
-            f = not f
+        res, A = [], sorted([[ch, hashT[ch]] for ch in hashT])
+        while len(res) != len(s):
+            i = 0
+            while len(A) > i:
+                if A[i][1] > 0:
+                    res.append(A[i][0])
+                A[i][1] -= 1
+                i += 1
+            i -= 1
+            while i > -1:
+                if A[i][1] > 0:
+                    res.append(A[i][0])
+                A[i][1] -= 1
+                i -= 1
         return ''.join(res)
