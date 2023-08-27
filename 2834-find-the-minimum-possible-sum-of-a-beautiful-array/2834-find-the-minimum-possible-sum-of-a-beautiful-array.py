953a3256857e6ecf4824f1f387634128d29009ab
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-27 20:38:09 +0530

    Time: 82 ms (66.67%), Space: 26.1 MB (33.33%) - LeetHub

--- 2834-find-the-minimum-possible-sum-of-a-beautiful-array/2834-find-the-minimum-possible-sum-of-a-beautiful-array.py
+++ 2834-find-the-minimum-possible-sum-of-a-beautiful-array/2834-find-the-minimum-possible-sum-of-a-beautiful-array.py
@@ -1,11 +1,9 @@
 class Solution:
     def minimumPossibleSum(self, n: int, target: int) -> int:
-        visit = set()
-        res, num = 0, 1
+        visit, num = set(), 1
         for _ in range(n):
             while target - num in visit:
                 num += 1
-            res += num
             visit.add(num)
             num += 1
-        return res
+        return sum(visit)
