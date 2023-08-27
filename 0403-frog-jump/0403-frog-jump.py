afa87752cb6a5841e9cdea0a8a2dc577709c783b
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-27 15:59:52 +0530

    Time: 115 ms (93.50%), Space: 24 MB (58.33%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -1,13 +1,12 @@
 class Solution:
-    def canCross(self, stones: List[int]) -> bool:
-        hashT = {val: i for i, val in enumerate(stones)}
+    def canCross(self, S: List[int]) -> bool:
+        A = {num: i for i, num in enumerate(S)}
         @cache
         def dfs(i, last):
-            if i >= len(stones) - 1: return True
+            if i == len(A)-1: return True
             res = False
-            for jump in [last - 1, last, last + 1]:
-                curr = stones[i] + jump
-                if curr in hashT and hashT[curr] > i:
-                    res = res or dfs(hashT[curr], jump)
+            for k in [last-1, last, last+1]:
+                if S[i]+k in A and i < A[k+S[i]]:
+                    res = res or dfs(A[k+S[i]], k)
             return res
         return dfs(0, 0)
