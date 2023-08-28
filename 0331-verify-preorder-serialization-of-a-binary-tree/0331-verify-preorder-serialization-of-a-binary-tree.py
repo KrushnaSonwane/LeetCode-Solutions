b5e2ea5abc901622ca37887284744f96b4a8c481
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-08-29 01:50:22 +0530

    Time: 47 ms (46.21%), Space: 16.4 MB (68.08%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.py
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.py
@@ -2,19 +2,17 @@ class Solution:
     def isValidSerialization(self, preorder: str) -> bool:
         A = preorder.split(',')
         if A[0]=='#': return A == ['#']
-        stack = [[A[0], 0]]
+        stack = [0]
         for i in range(1, len(A)):
             if A[i]=='#':
-                while stack and stack[-1][-1] == 2:
+                while stack and stack[-1] == 2:
                     stack.pop()
                 if not stack: return False
-                stack[-1][-1] += 1
+                stack[-1] += 1
             else:
-                while stack and stack[-1][-1] == 2:
+                while stack and stack[-1] == 2:
                     stack.pop()
                 if not stack: return False
-                stack[-1][1] += 1
-                stack.append([A[i], 0])
-        for _, val in stack:
-            if val != 2: return False
-        return True
+                stack[-1] += 1
+                stack.append(0)
+        return not any(val != 2 for val in stack)
