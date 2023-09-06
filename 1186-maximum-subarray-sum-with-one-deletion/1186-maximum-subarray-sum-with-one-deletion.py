dbe6deac92a75283c8fd2db4ab1470072fe07793
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-09-07 00:14:41 +0530

    Time: 542 ms (5.05%), Space: 247.3 MB (5.05%) - LeetHub

---
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumSum(self, A: List[int]) -> int:
+        @lru_cache(None)
+        def dfs(i, start, end, delete):
+            if i == len(A):
+                return 0 if start else -inf
+            if end: return 0
+            if not start:
+                res = dfs(i+1, start, end, delete)
+                res = max(res, A[i] + dfs(i+1, True, end, delete))
+            else:
+                res = A[i] + dfs(i+1, start, end, delete)
+                if not delete:
+                    res = max(res, dfs(i+1, start, end, True))
+                res = max(res, A[i] + dfs(i+1, start, True, delete))
+            return res
+        return dfs(0, False, False, False)
