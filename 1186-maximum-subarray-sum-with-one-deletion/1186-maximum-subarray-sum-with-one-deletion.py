a258b21cdc6f24bd1328e88c6d5bf7e6443d256c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-09-07 00:15:39 +0530

    Time: 562 ms (5.05%), Space: 247.1 MB (5.05%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
@@ -7,11 +7,11 @@ class Solution:
             if end: return 0
             if not start:
                 res = dfs(i+1, start, end, delete)
-                res = max(res, A[i] + dfs(i+1, True, end, delete))
+                res = max(res, A[i] + dfs(i+1, 1, end, delete))
             else:
                 res = A[i] + dfs(i+1, start, end, delete)
                 if not delete:
-                    res = max(res, dfs(i+1, start, end, True))
-                res = max(res, A[i] + dfs(i+1, start, True, delete))
+                    res = max(res, dfs(i+1, start, end, 1))
+                res = max(res, A[i] + dfs(i+1, start, 1, delete))
             return res
-        return dfs(0, False, False, False)
+        return dfs(0, 0, 0, 0)
