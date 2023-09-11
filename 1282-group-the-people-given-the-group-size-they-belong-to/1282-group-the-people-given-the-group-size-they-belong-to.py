ff015eb5262f82b244a09de6e044e66dbc78b2af
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-09-11 12:45:11 +0530

    Time: 76 ms (56.92%), Space: 16.3 MB (91.52%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,7 +1,6 @@
 class Solution:
     def groupThePeople(self, GS: List[int]) -> List[List[int]]:
-        GS = [[val, i] for i, val in enumerate(GS)]
-        GS.sort()
+        GS = sorted([[val, i] for i, val in enumerate(GS)])
         res, t = [], []
         for val, i in GS:
             t.append(i)
