9b18d139992936945ce8a6ef56925d12786d84f3
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-09-12 11:45:59 +0530

    Time: 118 ms (71.75%), Space: 17.2 MB (62.10%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        hashT, last = Counter(s), inf
+        A = sorted([val for val in hashT.values()])[::-1]
+        last, res = inf, 0
+        for a in A:
+            if last > a:
+                last = a
+            else:
+                if last == 0: 
+                    res += a
+                else:
+                    res += a - (last - 1)
+                    last -= 1
+        return res
