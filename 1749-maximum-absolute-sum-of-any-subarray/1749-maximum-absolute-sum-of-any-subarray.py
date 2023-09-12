edcb6cff6bdf1982d8b8d0db6a9914422955f147
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-09-12 18:40:44 +0530

    Time: 1082 ms (5.66%), Space: 350.2 MB (6.79%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
@@ -2,25 +2,23 @@ class Solution:
     def maxAbsoluteSum(self, A: List[int]) -> int:
         
         @lru_cache(None)
-        def dfs1(i, s, e):
+        def dfs1(i, s):
             if i == len(A):
                 return 0 if s else -inf
-            if e: return 0
             if s:
-                res = max(0, A[i] + dfs1(i+1,s,e))
+                res = max(0, A[i] + dfs1(i+1,s))
             else:
-                res = max(dfs1(i+1,s,e), A[i]+dfs1(i+1,1,e))
+                res = max(dfs1(i+1,s), A[i]+dfs1(i+1,1))
             return res
 
         @lru_cache(None)
-        def dfs2(i, s, e):
+        def dfs2(i, s):
             if i == len(A):
                 return 0 if s else inf
-            if e: return 0
             if s:
-                res = min(0, A[i] + dfs2(i+1,s,e))
+                res = min(0, A[i] + dfs2(i+1,s))
             else:
-                res = min(dfs2(i+1,s,e), A[i]+dfs2(i+1,1,e))
+                res = min(dfs2(i+1,s), A[i]+dfs2(i+1,1))
             return res
             
-        return max(dfs1(0,0,0), abs(dfs2(0,0,0)))
+        return max(dfs1(0,0), abs(dfs2(0,0)))
