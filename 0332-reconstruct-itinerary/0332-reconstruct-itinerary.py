22442a4ce3986577f047eb862f32855aec6bccf2
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-09-14 15:48:46 +0530

    Time: 88 ms (64.27%), Space: 16.9 MB (79.66%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,18 +1,14 @@
-class Solution(object):
-    def findItinerary(self, tickets):
-        """
-        :type tickets: List[List[str]]
-        :rtype: List[str]
-        """
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         hashT = defaultdict(list)
         for a, b in tickets:
             hashT[a].append(b)
         for a in hashT:
             hashT[a].sort()
-        def dfs(start):
-            while hashT[start]:
-                dfs(hashT[start].pop(0))
-            res.append(start)
         res = []
-        dfs("JFK")
+        def dfs(root):
+            while hashT[root]:
+                dfs(hashT[root].pop(0))
+            res.append(root)
+        dfs('JFK')
         return res[::-1]
