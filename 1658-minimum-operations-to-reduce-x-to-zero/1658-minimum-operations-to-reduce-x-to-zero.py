acb8349ed8282f368293d8795ecfa10a1526d040
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-09-21 00:56:53 +0530

    Time: 1063 ms (29.84%), Space: 38.2 MB (17.47%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,18 +1,15 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         if sum(nums) < x: return -1
-        lSum, i = 0, 0
-        while i < len(nums) and lSum + nums[i] <= x:
-            lSum += nums[i]
-            i += 1
-        res, j = i if lSum == x else inf, -1
-        i -= 1
-        rSum = 0
-        for j in range(len(nums)-1, -1, -1):
-            rSum += nums[j]
-            while i >= 0 and lSum + rSum > x:
-                lSum -= nums[i]
-                i -= 1
-            if lSum + rSum == x:
-                res = min(res, len(nums)-j + i + 1)
+        hashT, sum_, res = defaultdict(int), 0, inf
+        for i, num in enumerate(nums):
+            sum_ += num
+            if sum_ == x: res = i+1
+            hashT[sum_] = i
+        sum_, hashT[0] = 0, -1
+        for i in range(len(nums)-1, -1, -1):
+            sum_ += nums[i]
+            remain = x - sum_
+            if remain in hashT:
+                res = min(res, len(nums)-i+hashT[remain] + 1)
         return -1 if res == inf else res
