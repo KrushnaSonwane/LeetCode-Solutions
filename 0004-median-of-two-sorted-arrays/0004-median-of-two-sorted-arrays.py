492a65e47ccfca773d55dfdcb262517c09f92333
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-09-21 12:13:07 +0530

    Time: 62 ms (68.41%), Space: 13.4 MB (88.11%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        m, n = len(nums1), len(nums2)
+        i, j = 0, 0
+        for _ in range((m + n) // 2 - (1 if (m+n) % 2 == 0 else 0)):
+            if m > i and n > j:
+                if nums1[i] > nums2[j]:
+                    j += 1
+                else:
+                    i += 1
+            elif m > i:
+                i += 1
+            else:
+                j += 1
+        if (m+n) % 2:
+            if m > i and n > j:
+                return min(nums1[i], nums2[j]) / 1
+            elif m > i:
+                return nums1[i] / 1
+            return nums2[j] / 1 
+        else:
+            sum_ = 0
+            for _ in range(2):
+                if m > i and n > j:
+                    if nums1[i] > nums2[j]:
+                        sum_ += nums2[j]
+                        j += 1
+                    else:
+                        sum_ += nums1[i]
+                        i += 1
+                elif m > i:
+                    sum_ += nums1[i]
+                    i += 1
+                else:
+                    sum_ += nums2[j]
+                    j += 1
+            return sum_ / 2.00
