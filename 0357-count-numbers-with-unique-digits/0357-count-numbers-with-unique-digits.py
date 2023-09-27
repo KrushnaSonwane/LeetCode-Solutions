7ae24a7b651836121f5a705255a130f60884a09f
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-09-27 22:04:15 +0530

    Time: 75 ms (6.57%), Space: 16.9 MB (22.04%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.py
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.py
@@ -1,17 +1,20 @@
 class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
 
-        @lru_cache(None)
+        dp = {}
         def dfs(i, start, mask):
-            if i == n: return start
-            res = 0
-            if not start:
-                res = dfs(i+1, start, mask)
-            for d in range(1 if not start else 0, 10):
-                if mask[d] != '1':
-                    A = [ch for ch in mask]
-                    A[d] = '1'
-                    res += dfs(i+1, 1, ''.join(A))
-            return res
+            if (i, start, mask) not in dp:
+                if i == n: return start
+                res = 0
+                if not start:
+                    res = dfs(i+1, start, mask)
+                for d in range(1 if not start else 0, 10):
+                    if mask[d] != '1':
+                        A = [ch for ch in mask]
+                        A[d] = '1'
+                        res += dfs(i+1, 1, ''.join(A))
+                dp[(i, start, mask)] = res
+                return res
+            return dp[(i, start, mask)]
             
-        return dfs(0, False, '0'*20) + 1
+        return dfs(0, False, '0' * 10) + 1
