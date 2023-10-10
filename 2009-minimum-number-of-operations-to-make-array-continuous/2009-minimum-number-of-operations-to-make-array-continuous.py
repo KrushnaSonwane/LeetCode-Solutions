d17e9b7c8f15358ac0469b113674a6a26d4400ea
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-10 13:09:35 +0530

    Time: 907 ms (26.32%), Space: 38.6 MB (12.50%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
@@ -1,16 +1,16 @@
 class Solution:
-    def minOperations(self, nums: List[int]) -> int:
-        nums.sort()
+    def minOperations(self, A: List[int]) -> int:
+        A.sort()
         l, r, res = 0, 0, inf
         hashT = defaultdict(int)
         count = 0
-        while r < len(nums):
-            hashT[nums[r]] += 1
-            count += hashT[nums[r]] > 1
-            while nums[r] - nums[l] > len(nums)-1:
-                count -= hashT[nums[l]] > 1
-                hashT[nums[l]] -= 1
+        while r < len(A):
+            hashT[A[r]] += 1
+            count += hashT[A[r]] > 1
+            while A[r] - A[l] > len(A)-1:
+                count -= hashT[A[l]] > 1
+                hashT[A[l]] -= 1
                 l += 1
-            res = min(res, (len(nums) - (r - l + 1)) + count)
+            res = min(res, (len(A) - (r - l + 1)) + count)
             r += 1
         return res
