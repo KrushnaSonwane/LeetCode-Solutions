8393645a9de9b1d307d0ad78d2e717380d4f2ee0
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-11 14:33:57 +0530

    Time: 960 ms (43.33%), Space: 44.7 MB (50.61%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -1,11 +1,14 @@
-import bisect
-class Solution(object):
-    def fullBloomFlowers(self, F, P):
-        start = sorted(a for a, _ in F)
-        end = sorted(b for _, b in F)
-        n, res = len(F), []
-        for key in P:
-            l = bisect.bisect_right(start, key)
-            r = bisect.bisect_left(end, key)
-            res.append(l - r)
+class Solution:
+    def fullBloomFlowers(self, A: List[List[int]], P: List[int]) -> List[int]:
+        A.sort()
+        P = sorted([val, i] for i, val in enumerate(P))
+        Q, res, i = [], [0 for _ in P], 0
+        heapify(Q)
+        for key, j in P:
+            while i < len(A) and A[i][0] <= key:
+                heappush(Q, A[i][1])
+                i += 1
+            while Q and Q[0] < key:
+                heappop(Q)
+            res[j] = len(Q)
         return res
