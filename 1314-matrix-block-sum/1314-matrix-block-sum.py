887dfba41bf56828d0f745465364b4209f37c3ed
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-13 23:50:43 +0530

    Time: 5204 ms (7.12%), Space: 17.4 MB (84.11%) - LeetHub

---
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
+        print([[10]*100]*100)
+        m, n = len(mat), len(mat[0])
+        res = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                res[i][j] = sum(mat[r][c] for r in range(max(i - k, 0), min(m, i + k + 1)) for c in range(max(0, j - k), min(n, j+k+1)))
+        return res
