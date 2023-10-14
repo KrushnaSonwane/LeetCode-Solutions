c05756b4cd2785142bf67a02e54b7dd216117c66
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-14 09:41:28 +0530

    Time: 3789 ms (8.14%), Space: 750.3 MB (5.18%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.py
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -1,12 +1,11 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
-        n, dp = len(cost), {}
+        n = len(cost)
+        @lru_cache(None)
         def dfs(i, rem):
-            if i == len(cost):
+            if i == n:
                 return 0 if rem >= 0 else inf
-            if (i, rem) in dp: return dp[(i, rem)]
             take = dfs(i + 1, min(n, rem + time[i])) + cost[i]
             notTake = dfs(i + 1, rem - 1)
-            dp[(i, rem)] = min(take, notTake)
-            return dp[(i, rem)]
+            return min(take, notTake)
         return dfs(0, 0)
