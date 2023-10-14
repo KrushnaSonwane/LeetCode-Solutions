ad1edae9e891a6de3df7c6e552a6ad206ccd305c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-14 16:30:04 +0530

    Time: 1697 ms (27.84%), Space: 106.5 MB (35.05%) - LeetHub

--- 1223-dice-roll-simulation/1223-dice-roll-simulation.py
+++ 1223-dice-roll-simulation/1223-dice-roll-simulation.py
@@ -3,12 +3,7 @@ class Solution:
         MOD = 10**9+7
         @lru_cache(None)
         def dfs(i, last, count):
-            if count > rollMax[last-1]:
-                return 0
+            if count > rollMax[last-1]: return 0
             if i == n: return 1
-            res = 0
-            for num in range(1, 7):
-                res += dfs(i+1, num, 1 if num != last else count + 1)
-            res %= MOD
-            return res
+            return sum(dfs(i+1, num, 1 if num != last else count + 1) for num in range(1, 7)) % MOD
         return dfs(0, 0, 0)
