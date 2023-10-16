776e2cd2695fd2ef7b76f2dd4a59203283230ed8
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-16 15:22:42 +0530

    Time: 42 ms (46.15%), Space: 16.3 MB (69.82%) - LeetHub

--- 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
+++ 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
@@ -3,12 +3,12 @@ class Solution:
         A = str(n)
         @cache
         def dfs(i, start, tight):
-            if i == len(A): return 1 if start else 0
+            if i == len(A): return start
             res = 0
             if not start:
-                res = dfs(i+1, start, False)
+                res = dfs(i+1, 0, False)
             for d in digits:
                 if tight and d > A[i]: continue
-                res += dfs(i+1, True, tight and d == A[i])
+                res += dfs(i+1, 1, tight and d == A[i])
             return res
-        return dfs(0, False, True)
+        return dfs(0, 0, True)
