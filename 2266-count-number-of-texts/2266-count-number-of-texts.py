2e18064445fc2ffb28e83082b53e4720cb26bb5b
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-18 19:32:36 +0530

    Time: 1951 ms (14.80%), Space: 731.8 MB (5.10%) - LeetHub

---
+++ 2266-count-number-of-texts/2266-count-number-of-texts.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countTexts(self, A: str) -> int:
+        hashT = {'1': 1, '2': 3, '3': 3, '4': 3, '5': 3, '6': 3, '7': 4, '8': 3, '9': 4}
+        MOD = 10**9+7
+        @cache
+        def dfs(i, num, count):
+            if count > hashT[num]: return 0
+            if i == len(A): return 1
+            res = dfs(i+1, A[i], 1)
+            if A[i] == num:
+                res += dfs(i+1, num, count+1)
+            res %= MOD
+            return res
+        return dfs(0, '1', 0)
