d8c92e8441391cdd833cab901ac036e6f3c25f4e
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-19 11:31:54 +0530

    Time: 361 ms (94.12%), Space: 23.9 MB (79.93%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
@@ -3,9 +3,7 @@ class Solution:
         A, max_, res = defaultdict(int), 0, ''
         for msg, name in zip(messages, senders):
             A[name] += msg.count(' ') + 1
-            if A[name] > max_:
+            if (A[name] > max_) or A[name] == max_ and res < name:
                 max_ = A[name]
                 res = name
-            elif A[name]==max_:
-                res = max(res, name)
         return res
