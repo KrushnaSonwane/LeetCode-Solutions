ab6ae0e834efa63865ac7193c5c40d67b1e15c50
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-24 20:43:53 +0530

    Time: 298 ms (31.51%), Space: 85.8 MB (15.07%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.py
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.py
@@ -8,6 +8,4 @@ class Solution:
             res = max(res, dfs(i+2, rem-1, n) + slices[i])
             return res
         n = len(slices) // 3
-        res = dfs(1, n, len(slices))
-        slices.pop()
-        return max(res, dfs(0, n, len(slices)))
+        return max(dfs(1, n, len(slices)), dfs(0, n, len(slices)-1))
