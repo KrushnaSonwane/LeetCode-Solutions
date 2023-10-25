0106ce7edf873da8e39a2478254abeefd1cce402
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-25 19:27:10 +0530

    Time: 45 ms (11.73%), Space: 16.2 MB (79.64%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        A, temp = [], k
+        for i in range(n-1, 0, -1):
+            A.append(ceil(k / 2))
+            k = ceil(k/2)
+        def check(last, index):
+            if last == 0 and index % 2 == 0: 
+                return 1
+            if last and index % 2 == 0:
+                return 0
+            return last
+        A, res = A[::-1] + [temp], 0
+        for i in A[1:]:
+            res = check(res, i)
+        return res
