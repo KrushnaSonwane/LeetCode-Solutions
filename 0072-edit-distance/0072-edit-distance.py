ac3e94b11056eaba3f950341a853335d6662b009
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-10-30 23:29:06 +0530

    Time: 78 ms (95.23%), Space: 19.2 MB (78.69%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,15 +1,14 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m, n = len(word1), len(word2)
-        dp = {}
-        def dfs(i, j):
-            if (i, j) not in dp:
-                if j == n: return m - i
-                if i == m: return n - j
-                if word1[i] == word2[j]:
-                    res = dfs(i + 1, j + 1)
-                else:
-                    res = min(dfs(i, j + 1), dfs(i + 1, j), dfs(i + 1, j + 1)) + 1
-                dp[(i, j)] = res
-            return dp[(i, j)]
+        @cache
+        def dfs(x, y):
+            if x == len(word1):
+                return len(word2) - y
+            if y == len(word2):
+                return len(word1) - x
+            if word1[x] == word2[y]:
+                res = dfs(x + 1, y + 1)
+            else:
+                res = min(dfs(x, y+1), dfs(x+1, y), dfs(x+1, y+1)) + 1
+            return res
         return dfs(0, 0)
