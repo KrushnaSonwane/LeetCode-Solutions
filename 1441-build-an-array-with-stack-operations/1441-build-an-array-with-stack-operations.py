97d5e18343d8b63756198f3bae3af922f49ab351
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-11-03 11:34:27 +0530

    Time: 47 ms (10.30%), Space: 16.2 MB (44.85%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -5,12 +5,9 @@ class Solution:
             res.append("Pop")
         res, i = [], 0
         for num in range(1, min(target[-1], n) + 1):
-            if i < len(target):
-                if target[i] == num:
-                    res.append("Push")
-                    i += 1
-                else:
-                    operation()
+            if target[i] == num:
+                res.append("Push")
+                i += 1
             else:
                 operation()
         return res
