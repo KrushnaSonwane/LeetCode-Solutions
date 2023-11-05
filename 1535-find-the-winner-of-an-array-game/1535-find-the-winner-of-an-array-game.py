e67886b34106c03604a1c52ce4a497f6665f4c2d
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-11-05 14:58:15 +0530

    Time: 512 ms (89.67%), Space: 29.9 MB (63.04%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -2,10 +2,8 @@ class Solution:
     def getWinner(self, A: List[int], k: int) -> int:
         count = 0
         for i in range(1, len(A)):
-            if A[i] < A[0]:
-                count += 1
-            else:
-                A[0] = A[i]
-                count = 1
+            if A[i] > A[0]:
+                A[0], count = A[i], 0
+            count += 1
             if count == k: return A[0]
         return A[0]
