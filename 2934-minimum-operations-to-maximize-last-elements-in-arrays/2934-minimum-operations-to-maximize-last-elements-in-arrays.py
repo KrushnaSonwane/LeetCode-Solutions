4f55866251c105b18ef69cb55b962281740d38aa
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-11-14 23:12:38 +0530

    Time: 163 ms (72.26%), Space: 16.7 MB (32.81%) - LeetHub

--- 2934-minimum-operations-to-maximize-last-elements-in-arrays/2934-minimum-operations-to-maximize-last-elements-in-arrays.py
+++ 2934-minimum-operations-to-maximize-last-elements-in-arrays/2934-minimum-operations-to-maximize-last-elements-in-arrays.py
@@ -3,12 +3,9 @@ class Solution:
         def solve(A, B):
             res = 0
             for a, b in zip(A, B):
-                if A[-1] >= a and B[-1] >= b: 
-                    continue
-                elif A[-1] >= b and B[-1] >= a:
-                    res += 1
-                else:
-                    return inf
+                if A[-1] >= a and B[-1] >= b: continue
+                elif A[-1] >= b and B[-1] >= a: res += 1
+                else: return inf
             return res
         res = solve(A, B)
         A[-1], B[-1] = B[-1], A[-1]
