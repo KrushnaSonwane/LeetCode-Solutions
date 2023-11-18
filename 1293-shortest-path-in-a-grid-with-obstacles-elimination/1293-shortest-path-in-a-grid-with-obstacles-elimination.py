265615a03d1fcc165ecc2438df02017ef23a614c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-11-18 18:04:58 +0530

    Time: 723 ms (13.56%), Space: 23.7 MB (45.36%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -0,0 +1,17 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        visit, Q = {(0, 0, k)}, [[0, 0, 0, k]]
+        m, n = len(grid), len(grid[0])
+        while Q:
+            step, i, j, rem = heappop(Q)
+            if (i, j) == (m-1, n-1): return step
+            for x, y in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]:
+                if x in [-1, m]: continue
+                if y in [-1, n]: continue
+                if (x, y, rem) in visit: continue
+                visit.add((x,y,rem))
+                if grid[x][y] == 0:
+                    heappush(Q, [step+1, x, y, rem])
+                elif rem:
+                    heappush(Q, [step+1, x, y, rem-1])
+        return -1
