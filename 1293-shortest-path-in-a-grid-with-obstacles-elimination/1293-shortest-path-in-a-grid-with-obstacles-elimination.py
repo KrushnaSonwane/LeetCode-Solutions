3d3f1dd77007f8f9bf02dd3ebdbbb1c122bb2f3b
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-11-18 18:06:18 +0530

    Time: 685 ms (16.09%), Space: 23.8 MB (45.36%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -6,9 +6,7 @@ class Solution:
             step, i, j, rem = heappop(Q)
             if (i, j) == (m-1, n-1): return step
             for x, y in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]:
-                if x in [-1, m]: continue
-                if y in [-1, n]: continue
-                if (x, y, rem) in visit: continue
+                if x in [-1, m] or y in [-1, n] or (x, y, rem) in visit: continue
                 visit.add((x,y,rem))
                 if grid[x][y] == 0:
                     heappush(Q, [step+1, x, y, rem])
