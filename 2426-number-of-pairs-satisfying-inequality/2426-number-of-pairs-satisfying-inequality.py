205f739edd9d444e2fedda799221abcceba53d0b
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-11-21 16:30:00 +0530

    Time: 4589 ms (5.27%), Space: 35.3 MB (30.53%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -4,7 +4,6 @@ class Solution:
         nums, res = SortedList(), 0
         for a, b in zip(A, B):
             curr = -b + a + diff
-            l, r = 0, len(nums)-1
             res += bisect_right(nums, curr)
             nums.add(a - b)
         return res
