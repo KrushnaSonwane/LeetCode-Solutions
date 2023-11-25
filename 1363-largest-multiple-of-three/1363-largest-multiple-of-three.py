d3fc9ecb918f92e63308039b0b5d4249d6e67f1c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-11-25 18:10:16 +0530

    Time: 157 ms (46.53%), Space: 16.7 MB (76.24%) - LeetHub

---
+++ 1363-largest-multiple-of-three/1363-largest-multiple-of-three.py
@@ -0,0 +1,36 @@
+class Solution:
+    def largestMultipleOfThree(self, A: List[int]) -> str:
+        hashT = Counter(A)
+        if A.count(0) == len(A): return '0'
+        sum_ = sum(A)
+        mod = defaultdict(list)
+        for a in A:
+            mod[a%3].append(a)
+        if sum_%3==0: pass
+        elif sum_ % 3 == 1:
+            if len(mod[1]) > 0:
+                mod[1].sort()
+                hashT[mod[1][0]] -= 1
+            elif len(mod[2]) > 1:
+                mod[2].sort()
+                hashT[mod[2][0]] -= 1
+                hashT[mod[2][1]] -= 1
+            else:
+                return ''
+        else:
+            if len(mod[2]) > 0:
+                mod[2].sort()
+                hashT[mod[2][0]] -= 1
+            elif len(mod[1]) > 1:
+                mod[1].sort()
+                hashT[mod[1][0]] -= 1
+                hashT[mod[1][1]] -= 1
+            else:
+                return ''
+        res = []
+        for num in sorted(hashT.keys())[::-1]:
+            res.append(str(num)*hashT[num])
+        res = ''.join(res)
+        if not res: return ''
+        if res.count('0') == len(res): return '0'
+        return res
