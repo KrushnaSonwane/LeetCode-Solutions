c6eefd473cc3864f0545925ba1811fd59100b280
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-11-28 19:57:40 +0530

    Time: 336 ms (90.00%), Space: 17.3 MB (71.43%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -1,18 +1,19 @@
 class Solution:
     def numberOfWays(self, S: str) -> int:
-        A, sum_, count = [1], 0, 0
-        if S.count('S') % 2 or S.count('S') == 0: return 0
+        sum_, count = 0, 0
+        if S.count('S') % 2 or S.count('S') == 0: 
+            return 0
+        MOD, res = 10**9+7, 1
         for ch in S:
             if ch == 'S':
                 if sum_ == 2:
-                    A.append(count+1)
+                    res = (res * (count + 1)) % MOD
                     count, sum_ = 0, 1
                 else:
                     sum_ += 1
             else:
-                if sum_ == 2: count += 1
-                else: count = 0
-        res, MOD = 1, 10**9+7
-        for num in A:
-            res = (res * num) % MOD
+                if sum_ == 2: 
+                    count += 1
+                else: 
+                    count = 0
         return res
