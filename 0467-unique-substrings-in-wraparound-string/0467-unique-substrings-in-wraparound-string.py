34a910a482c2c53f34c1fe49c4abf0a70af48df4
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-12-13 20:34:01 +0530

    Time: 1102 ms (5.59%), Space: 17 MB (16.08%) - LeetHub

---
+++ 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findSubstringInWraproundString(self, S: str) -> int:
+        res = 0
+        for ch in range(97, 123, 1):
+            ch, count = chr(ch), 0
+            i = 0
+            while len(S) > i:
+                if S[i] == ch:
+                    t = 1
+                    while i + 1 < len(S):
+                        if S[i] != 'z':
+                            if ord(S[i]) + 1 != ord(S[i+1]):
+                                break
+                        else:
+                            if S[i+1] != 'a' or S[i] != 'z':
+                                break
+                        t, i = t + 1, i + 1
+                    count = max(count, t)
+                i += 1
+            res += count
+            print(count)
+        return res
