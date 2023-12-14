9a012b8870aee7ae61bf1e0d69772818ec2d303c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-12-14 22:21:40 +0530

    Time: 145 ms (80.03%), Space: 23.2 MB (26.97%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -3,7 +3,5 @@ class Solution:
         @cache
         def dfs(num):
             if num == 1: return 0
-            if num%2:
-                return 1 + dfs(num*3+1)
-            return 1 + dfs(num // 2)
+            return 1 + (dfs(num*3+1) if num % 2 else dfs(num//2))
         return sorted([[dfs(i), i] for i in range(lo, hi+1)])[k-1][1]
