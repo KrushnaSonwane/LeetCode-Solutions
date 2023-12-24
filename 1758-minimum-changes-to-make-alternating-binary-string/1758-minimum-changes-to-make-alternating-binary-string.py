a9a61b7f858849427370a3c7aba4d84a8ef86e09
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-12-24 09:18:16 +0530

    Time: 49 ms (69.33%), Space: 17.6 MB (5.67%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -3,9 +3,7 @@ class Solution:
         def solve(last):
             res = 0
             for ch in s:
-                if last and ch == '1':
-                    res += 1
-                elif not last and ch == '0':
+                if (last and ch == '1') or (not last and ch == '0'):
                     res += 1
                 last = not last
             return res
