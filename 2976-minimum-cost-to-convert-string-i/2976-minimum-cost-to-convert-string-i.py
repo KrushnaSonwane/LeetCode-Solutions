c545eb0cc24c9874fdfbb8182ccbfee1e64ebeb4
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-12-24 12:24:44 +0530

    Time: 1886 ms (100.00%), Space: 18.9 MB (50.00%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumCost(self, source: str, target: str, O: List[str], C: List[str], CC: List[int]) -> int:
+        A = [[inf for j in range(26)] for i in range(26)]
+        for i in range(26):
+            A[i][i] = 0
+        res = 0
+        for a, b, c in zip(O, C, CC):
+            a, b = ord(a)-97, ord(b)-97
+            A[a][b] = min(A[a][b], c)
+        for k in range(26):
+            for i in range(26):
+                for j in range(26):
+                    A[i][j] = min(A[i][j], A[i][k] + A[k][j])
+        for a, b in zip(source, target):
+            a, b = ord(a)-97, ord(b)-97
+            if A[a][b] == inf: return -1
+            res += A[a][b]
+        return res
