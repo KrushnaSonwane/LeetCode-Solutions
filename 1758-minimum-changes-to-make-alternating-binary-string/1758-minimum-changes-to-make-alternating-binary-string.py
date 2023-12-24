3f6c47aee1376f0427c16e9b20a8c37ff7d6e47c
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-12-24 09:17:38 +0530

    Time: 52 ms (56.72%), Space: 17.4 MB (5.67%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        def solve(last):
+            res = 0
+            for ch in s:
+                if last and ch == '1':
+                    res += 1
+                elif not last and ch == '0':
+                    res += 1
+                last = not last
+            return res
+        return min(solve(0), solve(1))
