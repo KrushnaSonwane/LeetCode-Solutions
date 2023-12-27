895ea44cac2d193fc45a4d37d3ba8858065edd69
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-12-27 23:11:18 +0530

    Time: 247 ms (71.19%), Space: 23.9 MB (5.32%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,9 +1,13 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        MOD = 10 ** 9 + 7
+        MOD = 10**9+7
         @cache
-        def dfs(rem, sum_):
-            if sum_ > target: return 0
-            if rem == 0: return int(sum_ == target)
-            return sum(dfs(rem - 1, sum_ + i) for i in range(1, k + 1))
-        return dfs(n, 0) % MOD
+        def dfs(i, sum_):
+            if i == n:
+                return 1 if sum_ == 0 else 0
+            res = 0
+            for num in range(1, k+1):
+                if sum_ >= num:
+                    res = (res + dfs(i+1, sum_ - num)) % MOD
+            return res
+        return dfs(0, target)
