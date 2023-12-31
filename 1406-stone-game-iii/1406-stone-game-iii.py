44bfa35515ccc5a6ef4d8fe2a6f0799b2ce57359
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2023-12-31 22:06:54 +0530

    Time: 7164 ms (5.12%), Space: 479.9 MB (14.76%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -1,19 +1,25 @@
-class Solution(object):
-    def stoneGameIII(self, stone):
-        """
-        :type stoneValue: List[int]
-        :rtype: str
-        """
-        dp, n = {}, len(stone)
-        def dfs(i):
-            if i >= n: return 0
-            if i in dp: return dp[i]
-            ans = stone[i] - dfs(i + 1)
-            if i + 1 < n:
-                ans = max(ans, stone[i] + stone[i + 1] - dfs(i + 2))
-            if i + 2 < n:
-                ans = max(ans, stone[i] + stone[i + 1] + stone[i + 2] - dfs(i + 3))
-            dp[i] = ans
-            return ans
-        res = dfs(0)
-        return 'Alice' if res > 0 else 'Bob' if res < 0 else 'Tie'
+class Solution:
+    def stoneGameIII(self, A: List[int]) -> str:
+        @cache
+        def dfs(i, alice):
+            if i == len(A):
+                return 0
+            if alice:
+                res = dfs(i+1, 0) + A[i]
+                if i + 1 < len(A):
+                    res = max(res, dfs(i+2, 0) + A[i]+A[i+1])
+                if i + 2 < len(A):
+                    res = max(res, dfs(i+3, 0) + A[i]+A[i+1]+A[i+2])
+            else:
+                res = dfs(i+1, 1) - A[i]
+                if i + 1 < len(A):
+                    res = min(res, dfs(i+2, 1) - (A[i]+A[i+1]))
+                if i + 2 < len(A):
+                    res = min(res, dfs(i+3, 1) - (A[i]+A[i+1]+A[i+2]))
+            return res
+        res = dfs(0, 1)
+        if res > 0:
+            return 'Alice'
+        elif res < 0:
+            return 'Bob'
+        return 'Tie'
