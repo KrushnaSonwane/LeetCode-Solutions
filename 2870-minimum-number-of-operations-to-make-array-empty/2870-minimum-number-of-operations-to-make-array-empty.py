c9630f9402ffa52e411a2f8e7971c16c5205806f
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-01-04 11:00:37 +0530

    Time: 862 ms (7.88%), Space: 138.6 MB (5.45%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -1,15 +1,17 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         nums.sort()
-        @lru_cache(None)
+        @cache
         def dfs(i):
-            if i == len(nums):
-                return 0
+            if i == len(nums): return 0
             res = inf
-            if i + 1 < len(nums) and nums[i+1]==nums[i]:
-                res = min(res, 1 + dfs(i+2))
-            if i + 1 < len(nums) and i + 2 < len(nums) and nums[i] == nums[i+1] == nums[i+2]:
-                res = min(res, 1 + dfs(i+3))
+            if i + 1 < len(nums):
+                if nums[i] == nums[i+1]:
+                    res = min(res, dfs(i+2) + 1)
+            if i + 2 < len(nums):
+                if nums[i] == nums[i+1] == nums[i+2]:
+                    res = min(res, dfs(i+3)+1)
             return res
-        res = dfs(0)
-        return -1 if res == inf else res
+        ans = dfs(0)
+        if ans == inf: return -1
+        return ans
