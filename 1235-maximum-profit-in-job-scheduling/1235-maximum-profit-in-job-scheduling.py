6df823f36e83bab9cf028872c1d06ee029bb762a
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-01-06 14:36:56 +0530

    Time: 817 ms (27.44%), Space: 84.7 MB (24.42%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,17 +1,20 @@
 class Solution:
-    def jobScheduling(self, S: List[int], E: List[int], P: List[int]) -> int:
-        A = sorted([[s, e, p] for s, e, p in zip(S, E, P)])
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        A = sorted([[s, e, p] for s, e, p in zip(startTime, endTime, profit)])
         @cache
         def dfs(i):
-            if i >= len(S): return 0
+            if i == len(A): return 0
             res = dfs(i+1)
-            l, r = 0, len(A)
-            while r > l:
-                mid = (r+l)//2
-                if A[mid][0] < A[i][1]:
-                    l = mid + 1
+            l, r = i+1, len(A)-1
+            ans = r+1
+            while r >= l:
+                mid = (r+l) // 2
+                s, e, p = A[mid]
+                if A[i][1] > s:
+                    l = mid  + 1
                 else:
-                    r = mid
-            res = max(res, dfs(r) + A[i][2])
+                    ans = mid
+                    r = mid - 1  
+            res = max(res, dfs(ans) + A[i][2])
             return res
         return dfs(0)
