81f6d82b36685156a4bb81f9f0a205a9e7014adc
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-01-20 22:52:19 +0530

    Time: 384 ms (48.88%), Space: 21.5 MB (62.41%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,29 +1,22 @@
 class Solution:
-    def sumSubarrayMins(self, arr: List[int]) -> int:
-        n = len(arr)
-        left = [-1] * n 
-        right = [n] * n
-        stack = []
-
-        for i, value in enumerate(arr):
-            while stack and arr[stack[-1]] >= value:  
-                stack.pop()  
-            if stack:
-                left[i] = stack[-1]  
-            stack.append(i) 
-
-        stack = [] 
-
+    def sumSubarrayMins(self, nums: List[int]) -> int:
+        n = len(nums)
+        left, right = [0 for _ in range(n)], [0 for _ in range(n)]
+        stack = [-1]
+        for i, num in enumerate(nums):
+            while len(stack) > 1 and nums[stack[-1]] >= num:
+                stack.pop()
+            left[i] = i - stack[-1]
+            stack.append(i)
+        stack = [n]
         
-        for i in range(n - 1, -1, -1):  
-            while stack and arr[stack[-1]] > arr[i]: 
-                stack.pop()  
-            if stack:
-                right[i] = stack[-1]  
-            stack.append(i) 
-
-        mod = 10**9 + 7 
-
-        result = sum((i - left[i]) * (right[i] - i) * value for i, value in enumerate(arr)) % mod
-      
-        return result 
+        for i in range(n-1, -1, -1):
+            while len(stack) > 1 and nums[stack[-1]] > nums[i]:
+                stack.pop()
+            right[i] = stack[-1] - i
+            stack.append(i)
+            
+        res, MOD = 0, 10**9+7
+        for i, num in enumerate(nums):
+            res = (res + (left[i] * right[i]) * num) % MOD
+        return res
