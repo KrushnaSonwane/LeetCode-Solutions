11948dfd7dab63a700f845978abd4f0c03eca597
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-01-25 22:23:17 +0530

    Time: 1168 ms (19.44%), Space: 310.5 MB (8.97%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,8 +2,7 @@ class Solution:
     def longestCommonSubsequence(self, A: str, B: str) -> int:
         @cache
         def dfs(i, j):
-            if len(A) == i: return 0
-            if len(B) == j: return 0
+            if len(A) == i or len(B) == j: return 0
             if A[i] == B[j]:
                 return dfs(i+1, j+1) + 1
             return max(dfs(i+1, j), dfs(i, j+1))
