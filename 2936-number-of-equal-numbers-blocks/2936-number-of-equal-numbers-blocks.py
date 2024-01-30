58b3328ff19a595ae21134f41650a6e89eff56c9
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-01-30 23:08:11 +0530

    Time: 16143 ms (16.54%), Space: 49.9 MB (54.33%) - LeetHub

---
+++ 2936-number-of-equal-numbers-blocks/2936-number-of-equal-numbers-blocks.py
@@ -0,0 +1,22 @@
+# Definition for BigArray.
+# class BigArray:
+#     def at(self, index: long) -> int:
+#         pass
+#     def size(self) -> long:
+#         pass
+class Solution(object):
+    def countBlocks(self, nums: Optional['BigArray']) -> int:
+        res, n = 0, nums.size()
+        i = 0
+        while i < n:
+            l, r = i, n-1
+            while r >= l:
+                mid = (r+l) // 2
+                if nums.at(mid) == nums.at(i):
+                    i = mid
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            res += 1
+            i += 1
+        return res
