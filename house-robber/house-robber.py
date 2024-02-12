c3e5bc371cf36b7dbdb5b311cf7e187147f0d5b9
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-02-12 21:20:31 +0530

    Time: 23 ms, Memory: 16.6 MB - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -1,15 +1,7 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        dp = [-1 for _ in nums]
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
         def dfs(i):
             if i >= len(nums): return 0
-            if dp[i] != -1: return dp[i]
-            take = nums[i] + dfs(i + 2)
-            notTake = dfs(i + 1)
-            dp[i] = max(take, notTake)
-            return dp[i]
+            return max(dfs(i+1), dfs(i+2) + nums[i])
         return dfs(0)
