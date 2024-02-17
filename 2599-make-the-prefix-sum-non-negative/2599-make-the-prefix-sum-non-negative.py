112f9d9fb0b02c1b6aef167299663ff11c0159b1
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-02-17 17:14:45 +0530

    Time: 711 ms (34.29%), Space: 31.3 MB (85.71%) - LeetHub

--- 2599-make-the-prefix-sum-non-negative/2599-make-the-prefix-sum-non-negative.py
+++ 2599-make-the-prefix-sum-non-negative/2599-make-the-prefix-sum-non-negative.py
@@ -1,14 +1,12 @@
-from sortedcontainers import SortedList
 class Solution:
     def makePrefSumNonNegative(self, nums: List[int]) -> int:
-        res, sum_, i = 0, 0, 0
-        A = SortedList()
+        res, sum_, A = 0, 0, []
         for i in range(len(nums)):
             num = nums[i]
             if num < 0:
-                A.add(num)
+                heappush(A, num)
             sum_ += num
             if sum_ < 0:
                 res += 1
-                sum_ -= A.pop(0)
+                sum_ -= heappop(A)
         return res
