77edea1684fdf04e38b5b2a6741690428dbcd528
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2024-02-18 07:14:46 +0530

    Time: 1185 ms (72.32%), Space: 62.8 MB (68.75%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -1,26 +1,21 @@
 class Solution:
-    def mostBooked(self, n: int, A: List[List[int]]) -> int:
-        Q, has, hashT = [], [], defaultdict(int)
-
-        for i in range(n):
-            heappush(has, i)
-        max_ = 0
-
-        for l, r in sorted(A):
-            while Q and Q[0][0] <= l or not has:
-                last, room = heappop(Q)
-                hashT[room] += 1
-                max_ = max(last, max_)
-                heappush(has, room)
-            heappush(Q, [max(max_+ (r-l), r), heappop(has)])
-
-        while Q: hashT[heappop(Q)[1]] += 1
-
-        res, count = inf, 0
-        for num in hashT:
-            if hashT[num] > count:
-                count = hashT[num]
-                res = num
-            elif hashT[num] == count:
-                res = min(res, num)
+    def mostBooked(self, n: int, M: List[List[int]]) -> int:
+        Q = [i for i in range(n)]
+        A, stack = Counter(), []
+        for l, r in sorted(M):
+            while stack and stack[0][0] <= l:
+                _, room = heappop(stack)
+                heappush(Q, room)
+            if Q:
+                room = heappop(Q)
+                A[room] += 1
+                heappush(stack, [r, room])
+            else:
+                end, room = heappop(stack)
+                A[room] += 1
+                heappush(stack, [max(l, end) + (r - l), room])
+        max_ = max(A.values())
+        res = inf
+        for num in A:
+            if max_ == A[num]: res = min(res, num)
         return res
